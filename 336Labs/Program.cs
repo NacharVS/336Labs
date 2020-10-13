407ee5eac271bf60d0150077ff2079d95ed221c3
r3dinc <r3dinc@yandex.ru>
2020-10-13 09:58:03 +0300

    fix

---
+++ 336Labs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _336Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
