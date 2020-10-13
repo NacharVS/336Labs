8a2b6535a51d1c62e33f9d0fcb569c7a36b5b5c3
Amir1902 <72787400+Amir1902@users.noreply.github.com>
2020-10-13 21:36:21 +0300

    Калькулятор

---
+++ Калькулятор Амир.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Калькулятор
+{
+    class Program
+
+
+    {
+
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        static double Dif(double a, double b)
+        {
+            return a - b;
+        }
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        static double Div(double a, double b)
+        {
+            return a / b;
+        }
+        static double VOZVIDENIE(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+        static double Koren(double a, double b)
+        {
+            return Math.Sqrt(a, b);
+        }
+        static void Main(string[] args)
+            {
+                Console.WriteLine("Введите числа:");
+                double a = Convert.ToDouble(Console.ReadLine());
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Операции:");
+                Console.WriteLine("Сумма: " + Sum(a, b));
+                Console.WriteLine("Разность: " + Dif(a, b));
+                Console.WriteLine("Умножение: " + Mul(a, b));
+                Console.WriteLine("Деление: " + Div(a, b));
+                Console.WriteLine("Возведение в степень: " + VOZVIDENIE(a, b));
+                Console.WriteLine("Корень из: " + Koren(a, b));
+            Console.ReadKey();
+
+            }
+        
+
+}   }    
