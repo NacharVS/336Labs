95a714b53570c4cbb7a2db1f52023a74f41869bd
Kolpakova336 <kke2003@mail.ru>
2020-10-15 01:40:03 +0300

    fix

--- 336Labs/Kolpakova/Calculator.cs
+++ 336Labs/Kolpakova/Calculator.cs
@@ -6,5 +6,50 @@ namespace _336Labs.Kolpakova
 {
     class Calculator
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Выберите операцию - sum/sub/div/pow/sqrt: ");
+            string SelectedAction = Console.ReadLine();
+            Console.WriteLine($"Введите число А: ");
+            double a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Введите число В: ");
+            int b = int.Parse(Console.ReadLine());
+            if (SelectedAction == "sqrt")
+            {
+                Console.WriteLine(Math.Sqrt(a));
+            }
+
+            else
+            {
+                switch (SelectedAction)
+                {
+                    case "sum":
+
+                        Console.WriteLine(a + b);
+                        break;
+                    case "sub":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "div":
+                        if (b == 0) Console.WriteLine($"На ноль делить нельзя!");
+                        else
+                        {
+                            Console.WriteLine(a / b);
+                        }
+                        break;
+                    case "pow":
+                        Console.WriteLine(a * b);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка");
+                        break;
+                        {
+                        }
+                }
+
+            }
+        }
     }
-}
+
+
+    }
