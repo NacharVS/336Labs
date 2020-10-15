421a263993f6a2cf41a30e716c35855a2c88935f
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-15 13:15:59 +0300

    fix

--- 336Labs/Galimzyanov/ClassesAndObjects.cs
+++ 336Labs/Galimzyanov/ClassesAndObjects.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace _336Labs.Galimzyanov
 {
+   static void Main(string[] args)
+    {
+
+
+
+    }
+
+    static int Array()
     class ClassesAndObjects
     {
+        int[,] array = new int[10, 10];
+        Random rnd = new Random();
+        for (int i=0; i<Array.GetLength(0);i++)
+
     }
 }
