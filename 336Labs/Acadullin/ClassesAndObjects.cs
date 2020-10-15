efe9c77c4831c4db076fe3b6ff89d2ae1a7607a1
AsadullinAizat336 <ayzat.asadullin2016@yandex.ru>
2020-10-15 22:42:44 +0300

    commit 3

--- 336Labs/Acadullin/ClassesAndObjects.cs
+++ 336Labs/Acadullin/ClassesAndObjects.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace _336Labs.Acadullin
 {
     class ClassesAndObjects
     {
-
-
+        static void Main(string[] args)
+        {
+            int[,] mass = new int[6, 6];
+        Random rnd = new Random();
+        int max = 0;
+        for (int i = 0; i<mass.GetLength(0); i++)
+            {
+               for (int j = 0; j<mass.GetLength(1); j++) 
+                {
+                 mass[i, j] = rnd.Next(10, 50);
+                 Console.Write($"{mass[i, j]} ");
+                    if (max<mass[i, j])
+                        max = mass[i, j];
+                }
+                Console.WriteLine();
+            }
+        }
+       
     }
-}
+}          
+
