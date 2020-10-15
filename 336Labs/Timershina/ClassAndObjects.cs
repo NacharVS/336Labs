423f61b1d4b15c1f469545bf3936e2012952172e
SabirovI <insaf.sabirov.02@mail.ru>
2020-10-15 13:19:16 +0300

    change start commit in lesson

--- 336Labs/Timershina/ClassAndObjects.cs
+++ 336Labs/Timershina/ClassAndObjects.cs
@@ -6,5 +6,32 @@ namespace _336Labs.Timershina
 {
     class ClassAndObjects
     {
+        static void Main(string[] args)
+        {
+            int[,] massive = new int[10, 5];
+            Random rnd = new Random();
+
+            for (int g = 0; g < massive.GetLength(0); g++)
+            {
+                for (int i = 0; i < massive.GetLength(0); i++)
+                {
+                    massive[g, i] = rnd.Next(10, 100);
+                    Console.WriteLine($"{massive[g, i]}$");
+
+                }
+                Console.WriteLine();
+
+            }
+            int sum = 0;
+            for (int i = 0; i < massive.GetLength(0); i++)
+            {
+                sum = massive[i, i] + sum;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
+
     }
 }
