7b86d72aab45902b65d9c05bb1d359efea5bbbfb
Sogorin <Sogorin_ivan@mail.ru>
2020-10-16 00:12:42 +0300

    v 0.6

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -6,8 +6,37 @@ namespace _336Labs
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
+            int[,] mas = new int[5, 5];
+            int[] su = new int[5];
+            Random r = new Random();
+            int s2 = 0;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                int l = i;
+                Console.Write($"{l} - ");
+                int sum = 0;
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {                   
+                    mas[i, j] = r.Next(0, 2);
+                    Console.Write($"{mas[i, j]} ");
+                    sum += mas[i, j];
+                }
+                Console.WriteLine("- summ row = " + sum);
+                for (int c = 0; c < su.GetLength(0); c++)
+                {
+                    if (sum > s2)
+                    {
+                        s2 = sum;
+                        su[c] = s2;
+                    }
+                    su[c] = sum;                    
+                }                                
+            }
+            for (int i = 0; i < su.GetLength(0); i++)
+            {
+                Console.Write(su[i]);
+            }
+            
         }
     }
 }
