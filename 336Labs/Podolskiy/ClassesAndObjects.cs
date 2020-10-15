577646aa5813ee107b458c819fcd2b0c6a5fb385
Podolskiy <daniilpodolskiy@mail.ru>
2020-10-15 13:20:05 +0300

    meet fix

---
+++ 336Labs/Podolskiy/ClassesAndObjects.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Podolskiy
+{
+    public static void main(String[] args)
+    {
+
+   
+
+    }
+    class ClassAndObjects
+    {
+         static  void Arrayfill()
+        {
+            int[,] mass = new int[10,10];
+            Random rand = new Random(); 
+            for(int i = 0; i<mass.Length; i++ )  {
+                for(int j = 0; j<mass.Length; j++)
+                {
+                    mass[i, j] = rand.Next(); 
+
+                }
+
+            }
+            static int Sum()
+            {
+
+            }
+
+        }
+    }
+
+}
+
+
