d14329b817a4592b3071831c421f2f1b0eaa73f6
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-10-15 12:29:00 +0300

    Method calculator add

---
+++ 336Labs/Ippolitova/MethodCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Ippolitova
+{
+    class MethodCalculator
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtracion(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        static int Square(int a)
+        {
+            return a * a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Addition(10, 5));
+            Console.WriteLine(Subtracion(10, 5));
+            Console.WriteLine(Multiplication(10, 5));
+            Console.WriteLine(Division(10, 5));
+            Console.WriteLine(Square(10));
+        }
+    }
+}
