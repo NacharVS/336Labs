f9f7e5df0be0b8396a60448d50aa09a91fccd3b9
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-15 13:15:06 +0300

    commit second

--- 336Labs/Bayburin/ClassesAndObject.cs
+++ 336Labs/Bayburin/ClassesAndObject.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace _336Labs.Bayburin
 {
+    class NumbersSumm
+    {
+        public int _number;
+        public int summ;
+    }
     class ClassesAndObject
     {
+        static void ArrayGeneration(int[] array)
+        {
+            int[,] mass = new int[5, 5];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+                Console.Write($"{array[i]} ");
+            }
+        }
     }
 }
