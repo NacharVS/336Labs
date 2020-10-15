062ac4f369936f2789625e0b2a6f3ebf6030c210
Liana949 <bekmuratovaliana2002@gmail.com>
2020-10-15 13:18:12 +0300

    fix

--- 336Labs/Bekmuratova/ClassesAndObjects.cs
+++ 336Labs/Bekmuratova/ClassesAndObjects.cs
@@ -6,5 +6,22 @@ namespace _336Labs.Bekmuratova
 {
     class ClassesAndObjects
     {
+        static void Gen(int[,] array, int g, int i)
+        {
+            Random rnd = new Random();
+
+            for (m = 0; m < array.GetLength(0); m++)
+            {
+                for (int n = 0; n < array.GetLength(0); n++)
+                {
+                    array[m, n] = rnd.Next(g, i);
+                    Console.WriteLine($"{array[m, n]} ");
+                }
+                Console.WriteLine("");
+
+
+            }
+            return;
+        }
     }
 }
