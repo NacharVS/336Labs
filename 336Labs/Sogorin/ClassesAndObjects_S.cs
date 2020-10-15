6e7eee4460d22a01316d69e4bf705d403ee5d0d7
Sogorin <Sogorin_ivan@mail.ru>
2020-10-15 13:21:25 +0300

    v 0.1

--- 336Labs/Sogorin/ClassesAndObjects_S.cs
+++ 336Labs/Sogorin/ClassesAndObjects_S.cs
@@ -6,5 +6,23 @@ namespace _336Labs.Sogorin
 {
     class ClassesAndObjects_S
     {
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[5, 5];
+            Random r = new Random();
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    mas[i, j] = r.Next(0, 1);
+                    sum = mas[i, j] + mas[i + 1, j];
+                    Console.WriteLine($"{mas[i, j]} ");
+                    Console.WriteLine("summ row = " + sum);
+                }
+            }
+
+        }
     }
 }
+
