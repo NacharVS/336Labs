101549f73f64a1f7b46e9fafb470dede3aacf726
AsadullinAizat336 <ayzat.asadullin2016@yandex.ru>
2020-10-15 17:38:40 +0300

    start

---
+++ 336Labs/Acadullin/MethodCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Acadullin
+{
+    class MethodCalculator
+    {
+        static double Sum(double a, double b)
+    }
+}
