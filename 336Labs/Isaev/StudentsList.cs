607d59d05af37010b23f57c7ef9af60c254ce7f1
VS1V <al11002@yndex.ru>
2020-10-16 11:03:35 +0300

    StudentList fix1

--- 336Labs/Isaev/StudentsList.cs
+++ 336Labs/Isaev/StudentsList.cs
@@ -2,9 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _336Labs.Isaev
+namespace _336Labs.Loginov
 {
     class StudentsList
     {
+
+        public string _name;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemistryMark;
+
+        public StudentsList(string name, double math, double physics, double chemisiry)
+        {
+            _name = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemistryMark = chemisiry;
+        }
+    }
+    class StudentSeletion
+    {
+        static void Selection(StudentsList[] list, double AveregeMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i]._mathMark + list[i]._physicsMark + list[i]._physicsMark + list[i]._chemistryMark / 3 >= AveregeMark)
+                {
+                    Console.WriteLine($"{list[i]._name} acces granted");
+                }
+            }
+        }
     }
 }
