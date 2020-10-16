784122d5019d934604444e291cf89c37378e995b
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-16 11:10:18 +0300

    commit 2

--- 336Labs/Bayburin/StudentsList.cs
+++ 336Labs/Bayburin/StudentsList.cs
@@ -6,5 +6,32 @@ namespace _336Labs.Bayburin
 {
     class StudentsList
     {
+        public string _namestudent;
+        public double _historyMark;
+        public double _mathMark;
+        public double _mdk0102Mark;
+        public double _mdk0103Mark;
+        public StudentsList(string namestudent, double historyMark, double mathMark, double mdk0102Mark, double mdk0103Mark)
+        {
+            _namestudent = namestudent;
+            _mathMark = mathMark;
+            _historyMark = historyMark;
+            _mdk0102Mark = mdk0102Mark;
+            _mdk0103Mark = mdk0103Mark;
+
+        }
+        class StudentMethod
+        {
+            public static void Method(StudentsList[] list, double AveregeMark)
+            {
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if ((list[i]._historyMark + list[i]._mathMark + list[i]._mdk0102Mark + list[i]._mdk0103Mark) / 4 >= AveregeMark)
+                    {
+                        Console.WriteLine($"{ list[i]._namestudent} допущен");
+                    }
+                }
+            }
+        }
     }
 }
