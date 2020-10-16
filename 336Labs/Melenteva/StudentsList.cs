b40a89fcb3b354b18b23f12c27d493ca8f11e3f3
Liana949 <bekmuratovaliana2002@gmail.com>
2020-10-16 10:44:25 +0300

    fix

--- 336Labs/Melenteva/StudentsList.cs
+++ 336Labs/Melenteva/StudentsList.cs
@@ -6,5 +6,31 @@ namespace _336Labs.Melenteva
 {
     class StudentsList
     {
+        public string _name;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemistryMark;
+
+        public StudentsList(string name, double math, double physics, double chemistry)
+        {
+            _name = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemistryMark = chemistry;
+        }
+    }
+
+    class StudentSelection
+    {
+        static void Selection(StudentsList[] list, double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((list[i]._mathMark + list[i]._physicsMark + list[i]._chemistryMark / 3 >= AverageMark))
+                {
+                    Console.WriteLine($"(list[i]._name) norm");
+                }
+            }
+        }
     }
 }
