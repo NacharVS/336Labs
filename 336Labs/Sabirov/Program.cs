956a8b80159766a7007c949307347347c0611b16
SabirovI <insaf.sabirov.02@mail.ru>
2020-10-16 11:03:28 +0300

    Start

---
+++ 336Labs/Sabirov/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _336Labs.Sabirov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudetsLists[] lists = new StudetsLists[5];
+            lists[0] = new StudetsLists("Ivan", 3, 3, 4);
+            lists[0] = new StudetsLists("Nastya", 5, 5, 5);
+            lists[0] = new StudetsLists("Masha", 2, 3, 4);
+            lists[0] = new StudetsLists("Bulat", 3, 2, 5);
+            lists[0] = new StudetsLists("Danil", 4, 3, 4);
+        }
+    }
+    static double Average(double math, double physics, double chemistry)
+    {
+        
+    }
+}
