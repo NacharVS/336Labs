35a3972391338fb320728b6315202fbedc2fdceb
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-16 10:26:48 +0300

    start

---
+++ 336Labs/Galimzyanov/Studentslist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Galimzyanov
+{
+    class Studentslist
+    {
+        public string _name;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemistryMark;
+
+    }
+}
