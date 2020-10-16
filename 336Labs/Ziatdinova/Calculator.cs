f999d1359f59f8a686fc99a6df3cc3ea3ff2e0d5
Kolpakova336 <kke2003@mail.ru>
2020-10-16 11:01:38 +0300

    fix

--- 336Labs/Ziatdinova/Calculator.cs
+++ 336Labs/Ziatdinova/Calculator.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace _336Labs.Ziatdinova
 {
-    class StudentsList
+    class Calculator
     {
     }
 }
---
+++ 336Labs/Ziatdinova/StudentsList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Ziatdinova
+{
+    class StudentsList
+    {
+        public string _name;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemistryMark;
+
+        public StudentsList(string name, double math, double physics, double chemistry)
+        {
+            _name = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemistryMark = chemistry;
+        }
+    }
+
+    class StudentSelection
+    {
+        static void Selection(StudentsList[] list, double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((list[i]._mathMark + list[i]._physicsMark + list[i]._chemistryMark) / 3 >= AverageMark)
+                {
+                    Console.WriteLine($"{list[i]._name} acces granted");
+                }
+            }
+        }
+    }
+}
+}
