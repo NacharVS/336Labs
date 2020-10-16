ce7b75bf3f16e899a4463a4e71245ad7d05093bc
Dolgov336 <dolgov20024@mail.ru>
2020-10-16 11:13:33 +0300

    StudentsList Fix

--- 336Labs/Dolgov/StudentsList.cs
+++ 336Labs/Dolgov/StudentsList.cs
@@ -6,5 +6,31 @@ namespace _336Labs.Dolgov
 {
     class StudentsList
     {
+        public string _name;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemisrtyMark;
+
+        public StudentsList(string name, double math, double physics, double chemistry)
+        {
+            _name = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemisrtyMark = chemistry;
+        }
+    }
+
+    class StudentsSelection
+    {
+        static void Selection(StudentsList[] list, double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((list[i]._mathMark + list[i]._physicsMark + list[i]._chemisrtyMark) / 3 >= AverageMark)
+                {
+                    Console.WriteLine($"{list[i]._name} acces granted ");
+                }
+            }
+        }
     }
 }
