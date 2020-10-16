0980b020861c1c82a32ed1b3bbd1928a5fe3947a
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-16 21:13:26 +0300

    Fin #2

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _336Labs.Galimzyanov;
+using System;
 
 namespace _336Labs
 {
@@ -6,7 +7,19 @@ namespace _336Labs
     {
         static void Main(string[] args)
         {
-            ClassesAndObjects.
+            Studentslist Azat = new Studentslist("Azat", 4, 5, 2);
+            Studentslist Amir = new Studentslist("Amir", 5, 4, 3);
+            Studentslist Aizat = new Studentslist("Aizat", 2, 2, 3);
+            Studentslist Adel = new Studentslist("Adel", 5, 4, 5);
+            Studentslist Bulat = new Studentslist("Bulat", 5, 5, 3);
+            Studentslist[] studentslists = new Studentslist[5];
+            studentslists[0] = Azat;
+            studentslists[1] = Amir;
+            studentslists[2] = Aizat;
+            studentslists[3] = Adel;
+            studentslists[4] = Bulat;
+            StudentSelection.Selection(studentslists, 4);
+
         }
     }
 }
