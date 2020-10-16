f4648443c4af688a4a5b1a711316cc6f4b2e0d75
Kolpakova336 <kke2003@mail.ru>
2020-10-16 10:32:59 +0300

    fix

---
+++ 336Labs/Kolpakova/ClassAndObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Kolpakova
+{
+    class ClassAndObjects
+    {
+        static void Massiv(int[,] array, int a, int b)
+        {
+            Random rndm = new Random();
+            for (int c = 0; c < array.GetLength(0); c++)
+            {
+                for (int d = 0; c < array.GetLength(0); d++)
+                {
+                    array { c, d} = rndm.Next(a, b);
+                    Console.Write($"[array{c,d}]");
+                }
+                Console.WriteLine("");
+            }
+            return;
+
+        }
+    }
+}
