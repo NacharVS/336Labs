c85acd7c75b0e1f8ee4e2c06c0105c1a9a46dfc0
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-16 10:47:49 +0300

    start

---
+++ 336Labs/Acadullin/StudentsList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Acadullin
+{
+    class StudentsList
+    {
+        public string _namestudent;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemistryMark;
+
+    }
+}
