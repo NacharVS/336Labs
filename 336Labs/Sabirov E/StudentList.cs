1c9f686c61d87f0c4e3b2b454c025b87593033e8
EmilSabirov336 <emil20020102@gmail.com>
2020-10-16 12:03:10 +0300

    StudentList fix

--- 336Labs/Sabirov E/StudentList.cs
+++ 336Labs/Sabirov E/StudentList.cs
@@ -6,5 +6,34 @@ namespace _336Labs.Sabirov_E
 {
     class StudentList
     {
+        public string _name;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemistryMark;
+
+        public StudentList(string name, double math, double physics, double chemistry)
+        {
+            _name = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemistryMark = chemistry;
+
+        }
     }
+    class StudentSelection
+    {
+        static void Selection(StudentList[] list,double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+
+            }
+        }
+    }
+
+
+
+
 }
+
+    
