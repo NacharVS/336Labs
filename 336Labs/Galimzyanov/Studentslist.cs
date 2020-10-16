0e117bef9d6ac992aeba33708af990a6e415a35b
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-16 11:05:00 +0300

    fix

--- 336Labs/Galimzyanov/Studentslist.cs
+++ 336Labs/Galimzyanov/Studentslist.cs
@@ -11,5 +11,22 @@ namespace _336Labs.Galimzyanov
         public double _physicsMark;
         public double _chemistryMark;
 
+        public Studentslist(string name, double math, double physics, double chemistry)
+        {
+            _name = name;
+            _mathMark = math;
+            _physicsMark = chemistry;
+        }
+    }
+    class StudentSelection
+    {
+        static void Selection(Studentslist [] list, double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i]._mathMark + list[i]._physicsMark + list[i]._chemistryMark) / 3 >== AverageMark)
+            }
+        }
     }
 }
+
