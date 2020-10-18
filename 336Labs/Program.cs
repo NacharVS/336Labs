2c317b40809138e654c11fb64eadc2338ca4a7c8
AndreySapegin336 <andreysapegin145@gmail.com>
2020-10-18 13:21:03 +0300

    StudentsList на паре почему-то не сохранилось, но там всё было как здесь

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _336Labs.Sapegin;
+using System;
+using System.Collections.Generic;
 
 namespace _336Labs
 {
@@ -6,8 +8,13 @@ namespace _336Labs
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
+            StudentsList[] list = new StudentsList[5];
+            list[0] = new StudentsList("Maksim", 4, 3, 4);
+            list[1] = new StudentsList("Ilya", 4, 5, 4);
+            list[2] = new StudentsList("Andrey", 5, 5, 4);
+            list[3] = new StudentsList("Leonid", 3, 3, 3);
+            list[4] = new StudentsList("Vasiliy", 4, 3, 5);
+            Console.WriteLine($" {list[i]._name} acces granted");
         }
     }
 }
