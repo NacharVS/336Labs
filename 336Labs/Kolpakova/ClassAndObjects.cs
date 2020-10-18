55255e8f15e6f0f131da7ad71953d245bda32139
Kolpakova336 <kke2003@mail.ru>
2020-10-18 20:44:20 +0300

    finish

--- 336Labs/Kolpakova/ClassAndObjects.cs
+++ 336Labs/Kolpakova/ClassAndObjects.cs
@@ -6,20 +6,35 @@ namespace _336Labs.Kolpakova
 {
     class ClassAndObjects
     {
-        static void Massiv(int[,] array, int a, int b)
+        static void Main(string[] args)
         {
-            Random rndm = new Random();
-            for (int c = 0; c < array.GetLength(0); c++)
+            Console.WriteLine("Сколько будет строк?");
+            Random rnd = new Random();
+            int len = Convert.ToInt32(Console.ReadLine()), sum = 0;
+            int[] array = new int[len];
+            for (int i = 0; i < len; i++)
             {
-                for (int d = 0; c < array.GetLength(0); d++)
-                {
-                    array { c, d} = rndm.Next(a, b);
-                    Console.Write($"[array{c,d}]");
-                }
-                C
+                array[i] = rnd.Next(0, 100);
+                sum += array[i];
+                Console.WriteLine((i + 1) + "-" + array[i]);
             }
-            
+            Console.WriteLine("Сумма цифр:" + sum);
+            int[] sortArray = new int[len];
+            Array.Copy(array, sortArray, len);
+            Array.Sort(sortArray);
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    if (sortArray[i] == array[j])
+                    {
+                        Console.WriteLine(j + 1);
+                    }
 
+                }
+
+
+            }
         }
     }
 }
