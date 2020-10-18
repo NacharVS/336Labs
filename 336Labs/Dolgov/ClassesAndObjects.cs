608932638d2ae3f005f154bcef1af6950c5e875b
Dolgov336 <dolgov200245@mail.ru>
2020-10-19 00:11:07 +0300

    ClassesAndObjects finish

--- 336Labs/Dolgov/ClassesAndObjects.cs
+++ 336Labs/Dolgov/ClassesAndObjects.cs
@@ -4,18 +4,32 @@ using System.Text;
 
 namespace _336Labs.Dolgov
 {
-    class chiselki
-    {
-        int maxchis;
-    }
+
     class ClassesAndObjects
     {
+        static void SortMas(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+            return;
+        }
         static void Gen(int[,] array, int a, int b)
         {
             Random rnd = new Random();
             for (int c = 0; c < array.GetLength(0); c++)
             {
-                for (int d = 0; c < array.GetLength(0); d++)
+                for (int d = 0; d < array.GetLength(1); d++)
                 {
                     array[c, d] = rnd.Next(a, b);
                     Console.Write($"{array[c, d]} ");
@@ -26,6 +40,25 @@ namespace _336Labs.Dolgov
         }
         public static void ClaAndObj()
         {
+
+            int sum = 0;
+            int[] mas1 = new int[10];
+            int[,] mas2 = new int[10, 10];
+            Gen(mas2, 0, 10);
+            for (int i = 0; i < mas2.GetLength(0); i++)
+            {
+                for (int n = 0; n < mas2.GetLength(1); n++)
+                {
+                    sum = sum + mas2[i, n];
+                }
+                mas1[i] = sum;
+                sum = 0;
+            }
+            SortMas(mas1);
+            for (int i = 0; i < mas1.Length; i++)
+            {
+                Console.WriteLine($"{i} - {mas1[i]}");
+            }
 
         }
     }
--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -8,14 +8,14 @@ namespace _336Labs
         static void Main(string[] args)
         {
             //MethodCalculator.Calculator();
-            //ClassesAndObjects.ClaAndObj();
+            ClassesAndObjects.ClaAndObj();
             StudentsList[] list = new StudentsList[5];
             list[0] = new StudentsList("Danil", 5, 4, 5);
             list[1] = new StudentsList("Bulat", 3, 5, 3);
             list[2] = new StudentsList("Maksim", 3, 3, 3);
             list[3] = new StudentsList("Vitya", 5, 5, 5);
             list[4] = new StudentsList("Nursinya", 4, 2, 3);
-            StudentsSelection.Selection(list, 3);
+            //StudentsSelection.Selection(list, 3);
         }
     }
 }
