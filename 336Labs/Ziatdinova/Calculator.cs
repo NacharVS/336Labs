fc0c02fa6eeee7c3ade7d42b62ef134be1c18c44
Ziatdinova336 <ziliya.ziatdinova@mail.ru>
2020-10-18 13:39:04 +0300

    Calculator fix

--- 336Labs/Ziatdinova/Calculator.cs
+++ 336Labs/Ziatdinova/Calculator.cs
@@ -6,6 +6,34 @@ namespace _336Labs.Ziatdinova
 {
     class Calculator
     {
-        Console.WriteLine
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введи число В");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выбери действие - summa/subtract/div/multip");
+            string SelectedAction = Console.ReadLine();
+
+        }
+
+
+        static int summa(int A, int B)
+        {
+            return A + B;
+        }
+        static int subtract(int A, int B)
+        {
+            return A - B;
+        }
+        static int div(int A, int B)
+        {
+            return A / B;
+        }
+        static int multip(int A, int B)
+        {
+            return A * B;
+        }
+        
     }
 }
