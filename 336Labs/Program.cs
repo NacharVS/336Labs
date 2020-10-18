f9101574e78840ba6b0685019d17a931b0695260
Kolpakova336 <kke2003@mail.ru>
2020-10-18 17:12:58 +0300

    zaBblLa zacomitit', sorry

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _336Labs.Kolpakova;
+using System;
 
 namespace _336Labs
 {
@@ -6,8 +7,14 @@ namespace _336Labs
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
+            StudentsList Kolpakova = new StudentsList("Kolpakova", 5, 4, 5);
+            StudentsList Ziatdinova = new StudentsList("Ziatdinova", 4, 5, 5);
+            StudentsList Zalyalov = new StudentsList("Zalyalov", 4, 3, 4);
+            StudentsList Galimzyanov = new StudentsList("Galimzyanov", 5, 3, 3);
+            StudentsList Daminov = new StudentsList("Daminov", 4, 5, 4);
+            StudentsList Dolgov = new StudentsList("Dolgov", 3, 2, 5);
+            StudentsList[] studentsLists = new StudentsList[6];
+
         }
     }
 }
