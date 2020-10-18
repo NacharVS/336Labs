b85a32b12b46b121f43b0fbc1cc7f9b5d360403b
Ainur-Farkhutdinov <farkhutdinovainur252@gmail.com>
2020-10-18 15:14:16 +0300

    program fix

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using _336Labs.Farkhutdinov;
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace _336Labs
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
         }
     }
