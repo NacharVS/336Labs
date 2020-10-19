1e20f1270d34a66b343dd8c9c284961dae7cb34c
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-19 22:38:07 +0300

    Fix #3 ClassesAndObjects

--- 336Labs/Galimzyanov/ClassesAndObjects.cs
+++ 336Labs/Galimzyanov/ClassesAndObjects.cs
@@ -1,25 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace _336Labs.Galimzyanov
-{ 
-  class ClassesAndObjects
+{
+    class ClassesAndObjects
     {
-        public static void Dvum(int[,] Array)
+        
+         public static void Dvum(int[,] Array)
+            
         {
+
             Random rnd = new Random();
             for (int i = 0; i < Array.GetLength(0); i++)
             {
-                for (int j = 0; j < Array.GetLength(0); j++)
+                for (int j = 0; j < Array.GetLength(1); j++)
                 {
                     Array[i, j] = rnd.Next(100);
-                    Console.WriteLine(Array[i, j]);
+                    Console.Write(Array[i, j] + " ");
                 }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < Array.GetLength(1); j++)
+
+                {
+                    int Sum = 0;
+                    Sum  += Array[i, j];
+                }
+                Console.WriteLine();
             }
 
         }
-
     }
 }
   
