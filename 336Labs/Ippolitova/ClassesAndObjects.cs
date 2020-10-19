758135285a452beda01e02aa0511603813f02788
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-10-19 10:48:34 +0300

    finish

--- 336Labs/Ippolitova/ClassesAndObjects.cs
+++ 336Labs/Ippolitova/ClassesAndObjects.cs
@@ -6,39 +6,67 @@ namespace _336Labs.Ippolitova
 {
     class ClassesAndObjects
     {
+        static void SortArray(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+            return;
+        }
+
+
         static void GenerationArray(int[,] arr, int MinN, int MaxN)
         {
             Random rnd = new Random();
             for (int i = 0; i < arr.GetLength(0); i++)
             {
-                for (int n = 0; n < arr.GetLength(0); n++)
+                for (int n = 0; n < arr.GetLength(1); n++)
                 {
                     arr[i, n] = rnd.Next(MinN, MaxN);
-                    Console.WriteLine($"{arr[i, n]} ");
+                    Console.Write($"{arr[i, n]} ");
                 }
                 Console.WriteLine(" ");
             }
             return;
         }
 
+
         public static void CaO()
         {
-            int max = 0;
-            int[,] array = new int[10, 10];
-            GenerationArray(array, 0, 10);
-            for (int i = 0; i < array.GetLength(0); i++)
+
+            int sum = 0;
+            int[] sumArr = new int[10];
+            int[] array2 = new int[10];
+            int[,] array1 = new int[10, 10];
+            GenerationArray(array1, 0, 10);
+            for (int i = 0; i < array1.GetLength(0); i++)
             {
-                for (int n = 0; n < array.GetLength(0); n++)
+                for (int n = 0; n < array1.GetLength(1); n++)
                 {
-                    if (max < array[i, n])
-                    {
-                        max = array[i, n];
-                    }
+                    sum = sum + array1[i, n];
+                    sumArr[i] = sum;
                 }
-                Console.WriteLine($"{i + 1} - {max}");
-                max = 0;
+                array2[i] = sum;
+                sum = 0;
             }
+            SortArray(array2);
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine($"{i} - {sumArr[i]}   {array2[i]}");
+            }
+
         }
+
 
     }
 }
