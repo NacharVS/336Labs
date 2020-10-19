080bb11aa12d9dff9bd2ac6f8b7d5063da71a53a
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-19 10:45:51 +0300

    start

---
+++ 336Labs/Acadullin/StringOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Acadullin
+{
+    class StringOperations
+    {
+        public static string _TEXT = "Калина это машина";
+
+        public static void 
+    }
+}
