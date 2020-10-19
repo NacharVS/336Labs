8152fbfb8d2b27d87e27532c4adbecd5e3dfc9a2
Ziatdinova336 <ziliya.ziatdinova@mail.ru>
2020-10-19 22:20:51 +0300

    StringOperations finish

--- 336Labs/Kolpakova/StringOperations.cs
+++ 336Labs/Kolpakova/StringOperations.cs
@@ -6,9 +6,27 @@ namespace _336Labs.Kolpakova
 {
     class StringOperations
     {
-        public static void StringOperations()
+        public static void Main(string[] args)
         {
             Console.WriteLine("Введите слово:");
+            string word = Console.ReadLine();
+            string oddLetters = "";
+            string evenLetters = "";
+            int index = 0;
+            foreach(char item in word)
+            {
+                if (index % 2 == 0)
+                {
+                    oddLetters += word[index];
+                }
+                else
+                {
+                    evenLetters += word[index];
+                }
+                index++;
+            }
+            Console.WriteLine("Нечетное " + oddLetters);
+            Console.WriteLine("Четное" + evenLetters);
            
             {
 
