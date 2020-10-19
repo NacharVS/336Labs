806a16319e2a982e9ac257345f4756583d4bc241
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-19 11:05:15 +0300

    fix

--- 336Labs/Farkhutdinov/StringOperations.cs
+++ 336Labs/Farkhutdinov/StringOperations.cs
@@ -6,5 +6,14 @@ namespace _336Labs.Farkhutdinov
 {
     class StringOperations
     {
+        public static string _letters = "Stewardesses";
+        public static void Delete(string Word)
+        {
+
+            Console.WriteLine(Word.Replace("ss", "s"));
+            Console.WriteLine(Word.Replace("es", "s"));
+            Console.WriteLine(Word.Replace("es", ""));
+        }
+
     }
 }
