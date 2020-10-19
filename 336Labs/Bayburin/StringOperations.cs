4c4e4fc5d08356ac1910a4bc1b2d444b00128a36
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-19 11:06:27 +0300

    commit 2

--- 336Labs/Bayburin/StringOperations.cs
+++ 336Labs/Bayburin/StringOperations.cs
@@ -6,5 +6,14 @@ namespace _336Labs.Bayburin
 {
     class StringOperations
     {
+        public static string _letters = "Stewardesses";
+        public static void Delete(string Word)
+        {
+
+            Console.WriteLine(Word.Replace("ss", "s"));
+            Console.WriteLine(Word.Replace("es", "s"));
+            Console.WriteLine(Word.Replace("es", ""));
+        }
+
     }
 }
