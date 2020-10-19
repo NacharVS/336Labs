74bd74bde1c2bde39574c003b907834b23970009
Liana949 <bekmuratovaliana2002@gmail.com>
2020-10-19 11:09:12 +0300

    fix

--- 336Labs/Bekmuratova/StringOperations.cs
+++ 336Labs/Bekmuratova/StringOperations.cs
@@ -6,5 +6,15 @@ namespace _336Labs.Bekmuratova
 {
     class StringOperations
     {
+      public static void StringOperations()
+        {
+            Console.WriteLine("write a word");
+            string_string = Console.ReadLine();
+            string_evenLeters = "";
+            string_oddLeters = "";
+            Console.WriteLine(_string);
+
+
+        }
     }
 }
