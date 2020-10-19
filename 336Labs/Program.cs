99e81175438b33c219548c989eb3800c37ef5ef5
Sogorin <Sogorin_ivan@mail.ru>
2020-10-19 10:41:13 +0300

    start

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -6,13 +6,9 @@ namespace _336Labs.Sogorin
     {
         static void Main(string[] args)
         {
-            StudentsList[] list = new StudentsList[5];
-            list[0] = new StudentsList("Ivan", 5, 5, 5);
-            list[0] = new StudentsList("Frank", 5, 4, 4);
-            list[0] = new StudentsList("Jop", 5, 5, 4);
-            list[0] = new StudentsList("Inga", 5, 3, 5);
-            list[0] = new StudentsList("Rev", 3, 5, 4);
-            Stsel.Sel(list, 4.0);
+            string s = Console.ReadLine();
+            
+            Console.WriteLine(s + 2);
         }
     }
 }
--- 336Labs/Sogorin/StudentsList.cs
+++ 336Labs/Sogorin/StudentsList.cs
@@ -32,3 +32,12 @@ namespace _336Labs.Sogorin
         }
     }
 }
+/*
+ *    StudentsList[] list = new StudentsList[5];
+            list[0] = new StudentsList("Ivan", 5, 5, 5);
+            list[0] = new StudentsList("Frank", 5, 4, 4);
+            list[0] = new StudentsList("Jop", 5, 5, 4);
+            list[0] = new StudentsList("Inga", 5, 3, 5);
+            list[0] = new StudentsList("Rev", 3, 5, 4);
+            Stsel.Sel(list, 4.0);
+ */
