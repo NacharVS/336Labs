3f4cab583cf11a967bd0f57c19c417bca34a9c0a
Kolpakova336 <kke2003@mail.ru>
2020-10-19 11:09:34 +0300

    StringOperations fix

--- 336Labs/Kolpakova/StringOperations.cs
+++ 336Labs/Kolpakova/StringOperations.cs
@@ -6,5 +6,16 @@ namespace _336Labs.Kolpakova
 {
     class StringOperations
     {
+        public static void StringOperations()
+        {
+            Console.WriteLine("Введите слово:");
+            string ab = Console.ReadLine();
+            string zi = "";
+            string ks = "";
+            while (ab != "")
+            {
+
+            }
+        }
     }
 }
