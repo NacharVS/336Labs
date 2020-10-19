68da9519359b0115c8bfc06030b60a8b6c7fb04a
AndreySapegin336 <andreysapegin145@gmail.com>
2020-10-19 23:57:02 +0300

    String Operations fix

--- 336Labs/Sapegin/StringOperations.cs
+++ 336Labs/Sapegin/StringOperations.cs
@@ -6,5 +6,26 @@ namespace _336Labs.Sapegin
 {
     class StringOperations
     {
+        public static void Substring(string[] A)
+        {
+
+            int A = 0;
+            int B = 0;
+            string words = "Ваша реклама";
+            Console.WriteLine(words);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i % 2 == 0) A = words[i];
+                else B = words[i];
+
+            }
+            Console.WriteLine(A);
+
+
+
+
+        }
+
     }
 }
+// я не понимаю, что выводится при компиляции
