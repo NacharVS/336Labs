48a5db4525ec38133eb3ec7f4721d3f263dc91dc
SabirovI <insaf.sabirov.02@mail.ru>
2020-10-19 11:01:41 +0300

    in the lesson

---
+++ 336Labs/Sabirov/StringOperations/StringOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Sabirov
+{
+    public class StringOperations
+    {
+        public static string _letters = "We want to zhrat";
+
+        public static void Concatenation(string s, string f)
+        {
+            Console.WriteLine(String.Concat(s, f));
+        }
+        public static void searchInLetters(string s, string f)
+        {
+            Console.WriteLine(s.IndexOf(f));
+        }
+    }
+    
+ 
+   
+    
+
+}
