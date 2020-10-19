cbee9966d9cd60b7d824781f64696835e5088720
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-10-19 11:05:51 +0300

    meet fix

--- 336Labs/Ippolitova/StringOperations.cs
+++ 336Labs/Ippolitova/StringOperations.cs
@@ -6,5 +6,13 @@ namespace _336Labs.Ippolitova
 {
     class StringOperations
     {
+        public static void ShowWords (string s)
+        {
+            string[] words = s.Split("wodrs words");
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
