07ff619fb5db9efa5ef013637324fda125af6591
TanyaBitkova336 <tanyabitkova116@gmail.com>
2020-10-19 10:36:54 +0300

    StringOperations start

---
+++ 336Labs/Bitkova/StringOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Bitkova
+{
+    class StringOperations
+    {
+        public static void Chetn_Nechetn()
+        {
+           string A = Console.ReadLine();
+            Console.WriteLine(A);
+            string[] words = A.Split(new char[] { ' ' });
+            
+
+
+
+        }
+    }
+}
