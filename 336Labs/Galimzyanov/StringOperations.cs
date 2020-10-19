5ff7cd691beb7733cdcb5ecef070e505214080da
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-19 21:59:24 +0300

    fin #4 ne zakoncheno

--- 336Labs/Galimzyanov/StringOperations.cs
+++ 336Labs/Galimzyanov/StringOperations.cs
@@ -6,7 +6,50 @@ namespace _336Labs.Galimzyanov
 {
     class StringOperations
     {
-        public static String _text = "I love shaurma";
-
+        //1
+       public static void StringRazdelit(string sg)
+        {
+            Console.Write("Нечетные: ");
+            for (int i = 0; i < sg.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(sg[i]);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Четные: ");
+            for (int i = 0; i < sg.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(sg[i]);
+                }
+            }
+        }
+        //2
+        public static void OdnaSymbol(string sg)
+        {
+            for (int i = 0; i < sg.Length; i++)
+            {
+                if (i+1 < sg.Length)
+                {
+                    if (sg[i] != sg[i + 1])
+                    {
+                        Console.Write(sg[i]);
+                    }
+                }
+            }
+            Console.Write(sg[sg.Length - 1]);
+        }
+       
+        //4 
+        public static bool Palindrom (string a)
+        {
+            for (int i = 1, j = a.Length - 1; i < j; i++, j--)
+                if (a[i] != a[j])
+                    return false;
+            return true;
+        }
     }
 }
