211f7afb0e5eaf5917998f265251616afe2b83e6
EmilSabiov336 <emil20020102@gmail.com>
2020-10-19 11:59:46 +0300

    StingOperations fix 1

--- 336Labs/Vasilev/StringOperations.cs
+++ 336Labs/Vasilev/StringOperations.cs
@@ -6,5 +6,27 @@ namespace _336Labs.Vasilev
 {
     class StringOperations
     {
+        public static void StrOpr()
+        {
+            Console.WriteLine("Введите строку");
+            string _string = Console.ReadLine();
+            string _evenLetters = "";
+            string _oddLetters = "";
+            Console.WriteLine(_string);
+            while (_string != "")
+            {
+                _oddLetters = _oddLetters + _string.Substring(0, 1);
+                _string = _string.Remove(0, 1);
+                if (_string != "")
+                {
+                    _evenLetters = _evenLetters + _string.Substring(0, 1);
+                    _string = _string.Remove(0, 1);
+                }
+            }
+            Console.WriteLine(_evenLetters);
+            Console.WriteLine(_oddLetters);
+        }
+
+
     }
 }
