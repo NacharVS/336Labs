4bf5c780fc464e139aa8deda0cea6953d0b1131a
Podolskiy <daniilpodolskiy@mail.ru>
2020-10-19 11:09:23 +0300

    FIX

--- 336Labs/Podolskiy/StringOperations.cs
+++ 336Labs/Podolskiy/StringOperations.cs
@@ -6,5 +6,29 @@ namespace _336Labs.Podolskiy
 {
     class StringOperations
     {
+        int count = 0;
+        public static void StringDestribute()
+        {
+            //string[] text = "Money";
+            //for (int i = 0; i < text.length; i++)
+            //{
+            //    if(i % 2 == 0)
+
+            string[] words = "я хочу кушать";
+            for (int i = 0; i < words.length; i++)
+            {
+                if (words[i] == 0)
+                {
+                    sum = sum + 1;
+                }
+            }
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        StringOperations StO = new StringOperations(); 
+        StringOperations.StringDestribute(); 
     }
 }
