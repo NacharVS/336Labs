fadc79de3c974b0dfb70142d5d8e7035648eadb8
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-19 10:32:00 +0300

    start

---
+++ 336Labs/Galimzyanov/StringOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Galimzyanov
+{
+    class StringOperations
+    {
+        public static String _text = "I love shaurma";
+
+    }
+}
