2f8934c1eec3a437bc2129433fcef0bd9394e1f9
Dolgov336 <dolgov20024@mail.ru>
2020-10-19 11:05:18 +0300

    StringOperations Fix

--- 336Labs/Dolgov/StringOperations.cs
+++ 336Labs/Dolgov/StringOperations.cs
@@ -6,5 +6,22 @@ namespace _336Labs.Dolgov
 {
     class StringOperations
     {
+        public static void StringOperations()
+        {
+            Console.WriteLine("Напишите строку");
+            string st = Console.ReadLine();
+            string ev = "";
+            string od = "";
+            while (st != "")
+            {
+                od = od + st.Substring(0, 1);
+                st = st.Remove(0, 1);
+                ev = ev + st.Substring(0, 1);
+                st = st.Remove(0, 1);
+            }
+            Console.WriteLine(st);
+            Console.WriteLine(od);
+            Console.WriteLine(ev);
+        }
     }
 }
