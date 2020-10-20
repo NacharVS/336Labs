ae570bb43a81865dcae5c149fcecda49f68a8918
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-20 22:36:10 +0300

    Fin#5

--- 336Labs/Galimzyanov/Studentslist.cs
+++ 336Labs/Galimzyanov/Studentslist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using System.Text;
 
 namespace _336Labs.Galimzyanov
@@ -7,16 +8,98 @@ namespace _336Labs.Galimzyanov
     class Studentslist
     {
         public string _name;
-        public double _mathMark;
-        public double _physicsMark;
-        public double _chemistryMark;
+        private double _mathMark;
+        private double _physicsMark;
+        private double _chemistryMark;
+
+        public double _MathMark
+        {
+            get 
+            { 
+                return _mathMark;
+            }
+            set
+            {
+                if (value < 6 && value > 1)
+                {
+                    this._mathMark = value;
+                }
+                else
+                {
+                     if (value > 5)
+                    {
+                        this._mathMark = 5;
+                    }
+                    else
+                    {
+                        this._mathMark = 2;
+                    }
+                }
+            }
+            
+        }
+        public double _PhysicsMark
+        {
+            get
+            {
+                return _physicsMark;
+            }
+            set
+            {
+                if (value < 6 && value > 1)
+                {
+                    this._physicsMark = value;
+                }
+                else
+                {
+                    if (value > 5)
+
+                    {
+                        this._physicsMark = 5;
+                    }
+                    else
+                    {
+                        this._physicsMark = 2;
+                    }
+                }
+            }
+        }
+        public double _ChemistryMark
+        {
+            get
+            {
+                return _chemistryMark;
+            }
+            set
+            {
+                if (value < 6 && value > 1)
+                {
+                    this._chemistryMark = value;
+                }
+                else
+                {
+                    if (value > 5)
+                    {
+                        this._chemistryMark = 5;
+                    }
+                    else
+                    {
+                        this._chemistryMark = 2;
+                    }
+
+                }
+                
+                    
+            }
+
+        }
 
         public Studentslist(string name, double math, double physics, double chemistry)
         {
             _name = name;
-            _mathMark = math;
-            _physicsMark = physics;
-            _chemistryMark = chemistry;
+            _MathMark = math;
+            _PhysicsMark = physics;
+            _ChemistryMark = chemistry;
         }
     }
     class StudentSelection
@@ -25,7 +108,7 @@ namespace _336Labs.Galimzyanov
         {
             for (int i = 0; i < list.Length; i++)
             {
-                if ((list[i]._mathMark + list[i]._physicsMark + list[i]._chemistryMark) / 3 >= AverageMark)
+                if ((list[i]._MathMark + list[i]._PhysicsMark + list[i]._ChemistryMark) / 3 >= AverageMark)
                 {
                     Console.WriteLine($"Студент под именем {list[i]._name} допущен к экзамену");
                 }
@@ -34,23 +117,6 @@ namespace _336Labs.Galimzyanov
                     Console.WriteLine($"Студент под именем {list[i]._name} не допущен к экзамену");
                 }
             }
-        }
-        private int AverageMark;
-
-        public int Ocenka 
-        {
-            set
-            {
-                if (value < 5)
-                {
-                    Console.WriteLine();
-                }
-                else
-                    {
-                    Ocenka = value;
-                    }
-            }
-            get { return Ocenka; }
         }
     }
 }
