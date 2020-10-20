06f1ac6e1b2d0ff9ea0914ccb9544f6c27024308
Kolpakova336 <kke2003@mail.ru>
2020-10-20 22:27:07 +0300

    StudentsList finish

--- 336Labs/Kolpakova/StudentsList.cs
+++ 336Labs/Kolpakova/StudentsList.cs
@@ -10,7 +10,10 @@ namespace _336Labs.Kolpakova
         public double _mathMark;
         public double _physicsMark;
         public double _chemistryMark;
-        public
+        public string Name { get => _name; set => _name = value; }
+        public double MathMark { get => _mathMark; set => _mathMark = CheckNoMore5(value); }
+        public double PhysicsMark { get => _physicsMark; set => _physicsMark = CheckNoMore5(value); }
+        public double ChemistryMark { get => _chemistryMark; set => _chemistryMark = CheckNoMore5(value); }
        
         public StudentsList(string name, double math, double physics, double chemistry)
         {
@@ -19,14 +22,37 @@ namespace _336Labs.Kolpakova
             _physicsMark = physics;
             _chemistryMark = chemistry;
         }
+        double CheckNoMore5(double value)
+        {
+            if(value > 5)
+            {
+                return 5;
+            }
+            else
+            {
+                return value;
+            }
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is StudentsList a)
+            {
+                return this.Name.CompareTo(a.Name);
+            }
+            else
+                throw new Exception("not students");
+        }
     }
 
     class StudentSelection
     {
         static void Selection(StudentsList[] list, double AverageMark)
         {
+            Array.Sort(list);
+            int index = 1;
             for (int i = 0; i < list.Length; i++)
             {
+                string strIndex = index > 1 ? $"{index}" : "";
                 if((list[i]._mathMark + list[i]._physicsMark + list[i]._chemistryMark) / 3 >= AverageMark)
                 {
                     Console.WriteLine($"Студент под именем {list[i]._name} допущен к экзамену");
@@ -34,6 +60,17 @@ namespace _336Labs.Kolpakova
                 else
                 {
                     Console.WriteLine($"Студент под именем {list[i]._name} не допущен к экзамену");
+                }
+                if (i + 1 < list.Length)
+                {
+                    if (list[i].Name == list [i + 1].Name)
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        index = 1;
+                    }
                 }
 
             }
