1ec5057e0352f8e096ddbe1495a7b7440cb41904
Dolgov336 <dolgov20024@mail.ru>
2020-10-21 10:26:19 +0300

    StudentsList start

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _336Labs.Zalyalov;
+using System;
 
 namespace _336Labs
 {
@@ -6,8 +7,13 @@ namespace _336Labs
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
+            StudentsList[] list = new StudentsList[5];
+            list[0] = new StudentsList("Omar", 4, 4, 5);
+            list[1] = new StudentsList("Bulat", 3, 4, 3);
+            list[2] = new StudentsList("Azat", 3, 3, 3);
+            list[3] = new StudentsList("Podol", 3, 2, 3);
+            list[4] = new StudentsList("Naimon", 4, 3, 4);
+            StudentsSelection.Selection(list, 3);
         }
     }
 }
---
+++ 336Labs/Zalyalov/StudentsList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Zalyalov
+{
+    class StudentsList
+    {
+        public string _namee;
+        public double _mathMark;
+        public double _physicsMark;
+        public double _chemisrtyMark;
+
+        public StudentsList(string name, double math, double physics, double chemistry)
+        {
+            _namee = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemisrtyMark = chemistry;
+        }
+    }
+
+    class StudentsSelection
+    {
+        public static void Selection(StudentsList[] list, double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((list[i]._mathMark + list[i]._physicsMark + list[i]._chemisrtyMark) / 4 >= AverageMark)
+                {
+                    Console.WriteLine($"{list[i]._namee} acces granted ");
+                }
+            }
+        }
+    }
+}
