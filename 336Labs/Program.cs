c699c6eca9ec0e92cfa895120b66bd28c9a6859b
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-21 11:01:30 +0300

    commit

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -9,6 +9,13 @@ namespace _336Labs
     {
          static void Main(string[] args)
         {
+            StudentsList.Method(new StudentsList[4], 4);
+            StudentsList[] list = new StudentsList[4];
+            list[0] = new StudentsList("azat", 5, 3, 5, 5);
+            list[1] = new StudentsList("azat", 5, 5, 5, 5);
+            list[2] = new StudentsList("ainur", 3, 3, 5, 5);
+            list[3] = new StudentsList("ainur", 2, 3, 2, 2);
+            list[4] = new StudentsList("amirlox", 3, 2, 2, 5);
         }
     }
 }
