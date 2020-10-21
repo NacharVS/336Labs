9302583f38acb8f7c2ff1c29a0fd57cdc341822e
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-21 11:07:18 +0300

    fix

--- 336Labs/Acadullin/StudentsList.cs
+++ 336Labs/Acadullin/StudentsList.cs
@@ -10,34 +10,9 @@ namespace _336Labs.Acadullin
         private double _mathMark;
         private double _physicsMark;
         private double _chemistryMark;
-
-        public StudentsList(string name, double math, double physics, double chemisrty)
+        public double MathMark
         {
-            _namestudent = name;
-            _mathMark = math;
-            _physicsMark = physics;
-            _chemistryMark = chemisrty;
-        }
-    }
-    class StudentSelection
-    {
-        //public static void Selection(StudentsList[] list, double AverageMark)
-        //{
-        //    for (int i = 0; i < list.Length; i++)
-        //    {
-        //        if ((list[i]._mathMark + list[i]._physicsMark + list[i]._chemistryMark) / 3 >= AverageMark)
-        //        {
-        //            Console.WriteLine($"Студент {list[i]._namestudent} с такими оценками допущен к экзамену!");
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine($"Студент {list[i]._namestudent} с такими оценками не допущен к экзамену!");
-        //        }
-        //    }
-        //}
-        public double _mathMark
-        {
-           get
+            get
             {
                 return _mathMark;
             }
@@ -47,18 +22,94 @@ namespace _336Labs.Acadullin
                 {
                     this._mathMark = value;
                 }
-                else 
+                else
                 {
                     if (value > 5)
                     {
                         this._mathMark = 5;
                     }
-                    else 
+                    else
                     {
                         this._mathMark = 2;
                     }
                 }
             }
+        }
+        public double PhysicsMark
+        {
+            get
+            {
+                return _physicsMark;
+            }
+            set
+            {
+                if (value < 6 && value > 1)
+                {
+                    this._physicsMark = value;
+                }
+                else
+                {
+                    if (value > 5)
+                    {
+                        this._physicsMark = 5;
+                    }
+                    else
+                    {
+                        this._physicsMark = 2;
+                    }
+                }
+            }
+        }
+        public double ChemistryMark
+        {
+            get
+            {
+                return _chemistryMark;
+            }
+            set
+            {
+                if (value < 6 && value > 1)
+                {
+                    this._chemistryMark = value;
+                }
+                else
+                {
+                    if (value > 5)
+                    {
+                        this._chemistryMark = 5;
+                    }
+                    else
+                    {
+                        this._chemistryMark = 2;
+                    }
+                }
+            }
+        }
+        public StudentsList(string name, double math, double physics, double chemisrty)
+        {
+            _namestudent = name;
+            _mathMark = math;
+            _physicsMark = physics;
+            _chemistryMark = chemisrty;
+        }
 }
+    class StudentSelection
+    {
+       
+        public static void Selection(StudentsList[] list, double AverageMark)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((list[i].MathMark + list[i].PhysicsMark + list[i].ChemistryMark) / 3 >= AverageMark)
+                {
+                    Console.WriteLine($"Студент {list[i]._namestudent} с такими оценками допущен к экзамену!");
+                }
+                else
+                {
+                    Console.WriteLine($"Студент {list[i]._namestudent} с такими оценками не допущен к экзамену!");
+                }
+            }
+        }
+
     }
 }
