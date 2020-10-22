e842f6a4dde9c8711461789f19eee4dd7888dc4c
Dolgov336 <dolgov20024@mail.ru>
2020-10-22 13:14:20 +0300

    BankAccount Fix

--- 336Labs/Dolgov/BankAccount.cs
+++ 336Labs/Dolgov/BankAccount.cs
@@ -6,6 +6,18 @@ namespace _336Labs.Dolgov
 {
     class BankAccount
     {
+        private string _name;
+        private string _surname;
+        private string _id;
+        public static double _rate = 0.067;
+        private double _paymentAccount;
 
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetters = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetters
+        }
     }
 }
