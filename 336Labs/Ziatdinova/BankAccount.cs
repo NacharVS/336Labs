53708f047d0c9042b7d0e06fca6ac9a55f1d79d1
Kolpakova336 <kke2003@mail.ru>
2020-10-22 13:13:00 +0300

    BankAccount fix

--- 336Labs/Ziatdinova/BankAccount.cs
+++ 336Labs/Ziatdinova/BankAccount.cs
@@ -6,5 +6,16 @@ namespace _336Labs.Ziatdinova
 {
     class BankAccount
     {
+        private string _name;
+        private string _SURNAME;
+        private string _id;
+        private static double _rate;
+        private double _paymentAccount;
+
+        public void SetName(string newName)
+        {
+         
+        }
     }
+
 }
