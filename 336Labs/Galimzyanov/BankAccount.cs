e416777190b887610795d839000de7c1170cfa64
Amir1902 <amir_galimzyanov@mail.ru>
2020-10-22 12:46:06 +0300

    start Bank

---
+++ 336Labs/Galimzyanov/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Galimzyanov
+{
+    class BankAccount
+    {
+        private string _name;
+        private string  _surname;
+        private string _id;
+        private static double _rate = 0.067;
+        private double _paymentAccount;
+
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetters = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper()
+        }
+
+
+    }
+}
