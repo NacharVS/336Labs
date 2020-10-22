bf395139ad3f9402c32c6344647934aa16477b6c
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-22 13:01:59 +0300

    commit 2

--- 336Labs/Bayburin/BankAccount.cs
+++ 336Labs/Bayburin/BankAccount.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace _336Labs.Bayburin
 {
-    class BankAccount
+    public class BankAccount
     {
+        private string name;
+        private string surname;
+        private string id;
+        private double payment;
+        private double contribution;
+        private double removal;
+        private static double rate = 7.7;
+
+        public client(string name, )
     }
+    
+
+    
 }
