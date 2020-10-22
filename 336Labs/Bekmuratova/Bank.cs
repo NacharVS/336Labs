65d3241e63d51e69f666545ad07919715870fe64
Liana949 <bekmuratovaliana2002@gmail.com>
2020-10-22 13:17:48 +0300

    fixx

--- 336Labs/Bekmuratova/Bank.cs
+++ 336Labs/Bekmuratova/Bank.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace _336Labs.Bekmuratova
 {
-    class Class1
+    class Bank
     {
+        private string name;
+        private string surname;
+        private string id;
+        private static double rate;
+        private double payementAccaunt;
+
+        public void SetName(string newlame)
+
+        {
+
+        }
     }
+
+
 }
--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -4,10 +4,22 @@ namespace _336Labs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Gen(int[,] array, int g, int i)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
+            Random rnd = new Random();
+
+            for (m = 0; m < array.GetLength(0); m++) 
+            {
+                for (int n = 0; n < array.GetLength(0); n++) 
+                {
+                    array[m, n] = rnd.Next(g, i);
+                    Console.WriteLine($"{array[m, n]} ");
+                }
+                Console.WriteLine("");
+
+
+            }
+            return;
         }
     }
 }
