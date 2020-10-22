41bc47071323f49d48fc31258a8fed7ca6bf8f73
Podolskiy <daniilpodolskiy@mail.ru>
2020-10-22 13:19:29 +0300

    fix

---
+++ 336Labs/Podolskiy/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Podolskiy
+{
+    class BankAccount
+    {
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static double _rate = 0.067;
+        public static double _paymentAccount;
+
+
+        public void setName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetters = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetters;
+
+        }
+        public void Deposit(double deposit)
+        {
+
+            _paymentAccount = _paymentAccount + deposit;
+            Console.WriteLine("На вашем счету " + _paymentAccount + " рублей ");
+        }
+        public void Withdraw(double withdraw)
+        {
+            _paymentAccount = _paymentAccount - withdraw;
+            Console.WriteLine("На вашем счету " + _paymentAccount + " рублей ");
+
+        }
+        static void main(string[] args)
+        {
+            BankAccount bank = new BankAccount();
+            BankAccount._rate = 0.089;
+            Console.WriteLine(BankAccount._rate); 
+            Acc
+
+            _paymentAccount = 100000;
+            bank.Deposit(1000);
+            bank.Withdraw(1500); 
+            
+        }
+    } 
+}
--- 336Labs/Podolskiy/NewStudentsList.cs
+++ 336Labs/Podolskiy/NewStudentsList.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace _336Labs.Podolskiy
 {
-    class NewStudentsList
+    class NewStudentList
     {
+        public string _name;
+        public double _MathMark;
+        public double _PhisMark;
+        public double _GeometryMark;
+
+
+
+        public NewStudentList(string name, double MathMark, double PhisMark, double GeometryMark)
+        {
+            _name = name;
+            _MathMark = MathMark;
+            _PhisMark = PhisMark;
+            _GeometryMark = GeometryMark;
+        }
+
+    }
+    class List
+    {
+        internal static void Method(StudentList[] lists, double AveregeMark)
+        {
+            for (int i = 0; i < lists.Length; i++)
+            {
+                if (lists[i]._GeometryMark + lists[i]._GeometryMark + lists[i]._MathMark + lists[i]._PhisMark / 3 >= AveregeMark)
+
+                    Console.WriteLine($"{lists[i]._name} access granted");
+
+            }
+        }
+    }
+
+    class Person
+    {
+        StudentList list = new NewStudentList();
+
+
+        private double mark = Convert.ToDouble(Console.ReadLine());
+        public double Mark
+        {
+            set
+            {
+                if (mark > list._GeometryMark)
+                {
+                    mark = 5;
+                    Console.WriteLine(mark);
+                }
+
+            }
+            get
+            {
+                return mark;
+            }
+        }
     }
 }
