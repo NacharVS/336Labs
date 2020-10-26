42237a1a3bd365190b6db80d8a38c7e3ea84f551
NailYS <nail.yusupov.08@bk.ru>
2020-10-27 00:43:28 +0400

    FINISHED

---
+++ 336Labs/Yusupov/StringOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Yusupov
+{
+    class StringOperation
+    {
+        public static void main (string[] args )
+        {
+            Console.WriteLine("Введите слово:");
+            string word = Console.ReadLine();
+            string oddLetters = "";
+            string evenLetters = "";
+            int parametr  = 0; 
+            foreach(char item in word  )
+            {
+               if (parametr % 2 ==0 )
+                {
+                    oddLetters += word[parametr ]; 
+
+                }else
+                {
+                    evenLetters += word[parametr]; 
+                }
+                parametr++; 
+            }
+            Console.WriteLine("нечетное " + oddLetters );
+            Console.WriteLine("четное "+  evenLetters );
+        }
+    }
+}
