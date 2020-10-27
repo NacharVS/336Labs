558e19b553e51e83e6d6943f6fd7b6cdea2b9e81
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-27 10:59:31 +0300

    commit fix 27.10.2020

--- 336Labs/Farkhutdinov/BankAccount.cs
+++ 336Labs/Farkhutdinov/BankAccount.cs
@@ -4,17 +4,13 @@ using System.Text;
 
 namespace _336Labs.Farkhutdinov
 {
-    class BankAccoun
-    { 
-        {
-            private string _name;
-            private string _surname;
-            private string _id;
-            private double _payment;
-            private double _contribution;
-            private double _removal;
-            private static double _rate = 7.7;
-        
+    class BankAccount
+    {
+        private string _name;
+        private string _surname;
+        private static int _id = Environment.TickCount;
+        private static double _rate = 7.7;
+
         public void SetName(string newName)
         {
             newName = newName.Trim();
@@ -23,6 +19,17 @@ namespace _336Labs.Farkhutdinov
             _name = firstLetters.ToString().ToUpper() + otherLetters;
         }
 
+        public void SetSurName(string newSurName)
+        {
+            newSurName = newSurName.Trim();
+            var firstLetters = newSurName[0];
+            var otherLetters = newSurName.Remove(0, 1);
+            _surname = firstLetters.ToString().ToUpper() + otherLetters;
+        }
 
+        public int personid
+        {
+            get { return Interlocked.Increment(ref _id); }
+        }
     }
 }
