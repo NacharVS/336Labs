644d4b1f44ae0aa66dc0478ade8efe1dc7e5d091
Podolskiy <daniilpodolskiy@mail.ru>
2020-10-27 10:27:26 +0300

    FIX

---
+++ 336Labs/Podolskiy/DateTime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Podolskiy
+{
+    class DATETime
+    {
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static double _rate = 0.021;
+        private double _paymentAccount = 12000;
+
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetter = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetter;
+        }
+        public void Deposit()
+        {
+            Console.WriteLine("Сколько хотите внести >>> ");
+
+            double depos = Convert.ToDouble(Console.ReadLine());
+
+            _paymentAccount = _paymentAccount + depos;
+
+            Console.WriteLine("На вашем счету стало " + _paymentAccount);
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Сколько хотите снять  >>> ");
+            double withdraw = Convert.ToDouble(Console.ReadLine());
+            _paymentAccount = _paymentAccount - withdraw;
+
+            Console.WriteLine("На вашем счету осталось : " + _paymentAccount);
+        }
+    }
+}
--- 336Labs/Podolskiy/Programm.cs
+++ 336Labs/Podolskiy/Programm.cs
@@ -1,21 +1,16 @@
-﻿using System;
+﻿using _336Labs.Podolskiy;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace _336Labs.Podolskiy
+namespace _336Labs
 {
-    class Programm
+    class Program
     {
         static void Main(string[] args)
         {
-            StudentList[] list = new StudentList[5];
-            list[0] = new StudentsList("Ivan", 5, 3, 4);
-            list[1] = new StudentsList("Victor", 3, 3, 4);
-            list[2] = new StudentsList("Ekaterina", 5, 5, 5);
-            list[3] = new StudentsList("Idris", 5, 4, 4);
-            list[4] = new StudentsList("Kolyan", 3, 3, 3);
-
-            StudentSelection.Selection(list, 3.9);
+            DATETime time = new DATETime();
+            time.Deposit();
+            time.Withdraw(); 
         }
     }
 }
