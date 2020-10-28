4d4e61b5f6d839d5328384646dc9f8258f7ff961
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-10-28 11:03:03 +0300

    finish

--- 336Labs/Ippolitova/BankAccount.cs
+++ 336Labs/Ippolitova/BankAccount.cs
@@ -11,8 +11,13 @@ namespace _336Labs.Ippolitova
         private string _id;
         private double _paymentAccount = 2000;
         private double _age;
+        private DateTime DayOfBirth = new DateTime();
 
-        public void SetName(string newName)
+        
+        DateTime dayofbirth = DateTime.Now.Date;
+    
+
+    public void SetName(string newName)
         {
             newName = newName.Trim();
             var firstLetter = newName[0];
@@ -41,6 +46,30 @@ namespace _336Labs.Ippolitova
             Console.WriteLine("На вашем счету осталось : " + _paymentAccount);
         }
 
+        public static void SetAge(BankAccount bank)
+        {
+            Console.WriteLine("Введите год рождения");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц рождения");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите день рождения");
+            int day = Convert.ToInt32(Console.ReadLine());
+            bank.DayOfBirth = new DateTime(year, month, day);
+            Console.WriteLine(bank.DayOfBirth);
+            DateTime Today = DateTime.Now;
+            Console.WriteLine(Today);
+            bank._age = Today.Year - bank.DayOfBirth.Year;
+            Console.WriteLine(bank._age);
+        }
+
+        public static void GetNameSurnameId(BankAccount bank)
+        {
+            Console.WriteLine($"Имя:     {bank._name}");
+            Console.WriteLine($"Фамилия: {bank._surname}");
+            Console.WriteLine($"ID:      {bank._id}");
+            Console.WriteLine($"Дата рождения: {bank.DayOfBirth.ToString("dd:MM:yyyy")}");
+            Console.WriteLine($"Возраст: {bank._age}");
+        }
 
     }
 }
