87e68c697189f7bf680ed8d44869c6e2921d72dd
VitaliiLoginov <oblagor@gmail.com>
2020-10-28 11:56:40 +0300

    BankAccount fix

--- 336Labs/Loginov/BankAccount.cs
+++ 336Labs/Loginov/BankAccount.cs
@@ -6,13 +6,13 @@ namespace _336Labs.Loginov
 {
     class BankAccount
     {
+        //Переменные
         private string _name;
         private string _surname;
         private int _id, _age;
-        public static double _rate = 0.067;
+        private static double _rate = 0.67;
         private double _paymentAccount;
         private DateTime DayOfBirth = new DateTime();
-
 
         //Установка
         public void SetName(string newName)
@@ -31,15 +31,12 @@ namespace _336Labs.Loginov
             _surname = firstLetter.ToString().ToUpper() + otherLetters.ToString().ToLower();
         }
 
-        public void SetAge(DateTime newAge)
-        {
-            DateTime dayofbirth = DateTime.Now.Date;
-        }
         public static void SetId(BankAccount bank)
         {
             Random rnd = new Random();
             bank._id = rnd.Next(10, 100);
         }
+
         public static void SetNameSurnameIdAge(BankAccount bank)
         {
             Console.WriteLine("Введите имя");
@@ -47,6 +44,7 @@ namespace _336Labs.Loginov
             Console.WriteLine("Введите фамилию");
             bank.SetSurname(Console.ReadLine());
         }
+
         public static void SetAge(BankAccount bank)
         {
             Console.WriteLine("Введите год рождения");
@@ -56,43 +54,52 @@ namespace _336Labs.Loginov
             Console.WriteLine("Введите день рождения");
             int day = Convert.ToInt32(Console.ReadLine());
             bank.DayOfBirth = new DateTime(year, month, day);
-            Console.WriteLine(bank.DayOfBirth);
             DateTime Today = DateTime.Now;
-            Console.WriteLine(Today);
             bank._age = Today.Year - bank.DayOfBirth.Year;
-            Console.WriteLine(bank._age);
+            Console.WriteLine();
         }
 
-
         //Выдача
-        public static void GetNameSurnameId(BankAccount bank)
+        public static bool GetNameSurnameIdAgeRate(BankAccount bank)
         {
-            Console.WriteLine($"Имя:     {bank._name}");
-            Console.WriteLine($"Фамилия: {bank._surname}");
-            Console.WriteLine($"ID:      {bank._id}");
-            Console.WriteLine($"Дата рождения: {bank.DayOfBirth.ToString("dd:MM:yyyy")}");
-            Console.WriteLine($"Возраст: {bank._age}");
+            if (bank._age < 14)
+            {
+                Console.WriteLine("Вы не можете завести счет в банке");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine($"Имя:     {bank._name}");
+                Console.WriteLine($"Фамилия: {bank._surname}");
+                Console.WriteLine($"ID:      {bank._id}");
+                Console.WriteLine($"Дата рождения: {bank.DayOfBirth.ToString("dd:MM:yyyy")}");
+                Console.WriteLine($"Возраст: {bank._age}");
+                Console.WriteLine($"Ставка: {_rate}");
+                return true;
+            }
         }
         // Операции
         public void Deposit()
         {
-            Console.WriteLine("Сколько хотите внести >>> ");
+            Console.Write("Сколько хотите внести >>> ");
 
             double depos = Convert.ToDouble(Console.ReadLine());
 
             _paymentAccount = _paymentAccount + depos;
 
-            Console.WriteLine("На вашем счету стало " + _paymentAccount);
+            Console.Write("На вашем счету стало " + _paymentAccount);
         }
 
         public void Withdraw()
         {
-            Console.WriteLine("Сколько хотите снять  >>> ");
+            Console.Write("Сколько хотите снять  >>> ");
             double withdraw = Convert.ToDouble(Console.ReadLine());
             _paymentAccount = _paymentAccount - withdraw;
-            Console.WriteLine("На вашем счету осталось : " + _paymentAccount);
+            Console.Write("На вашем счету осталось : " + _paymentAccount);
         }
     }
+
+
 
     class BA
     {
@@ -103,12 +110,16 @@ namespace _336Labs.Loginov
             BankAccount.SetNameSurnameIdAge(bank);
             BankAccount.SetId(bank);
             BankAccount.SetAge(bank);
-            BankAccount.GetNameSurnameId(bank);
-            int n = 1;
-            while (n >0)
+            if (BankAccount.GetNameSurnameIdAgeRate(bank) == true)
             {
-                bank.Deposit();
-                bank.Withdraw();
+                int n = 0;
+                while (n == 0)
+                {
+                    Console.WriteLine();
+                    bank.Deposit();
+                    Console.WriteLine();
+                    bank.Withdraw();
+                }
             }
         }
     }
