a2488af29960cca4e2878132ca677c5c20763e05
Sogorin <Sogorin_ivan@mail.ru>
2020-10-28 10:21:20 +0300

    V0.02 Sogorin and Didikova

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 
-namespace _336Labs
+namespace _336Labs.didikova
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
+            Account account = new Account();
+            BankAcc[] Table = new BankAcc[10];
+            Table[0] = new BankAcc("Kuzak", "pvan");
+            Table[1] = new BankAcc("Kuzap", "uvan");
+            Table[2] = new BankAcc("Kuzam", "tvan");
+            Table[3] = new BankAcc("Kuzan", "yvan");
+            Table[4] = new BankAcc("Kuzab", "evan");
+            Table[5] = new BankAcc("Kuzac", "qvan");
+            Table[6] = new BankAcc("Kuzax", "dvan");
+            Table[7] = new BankAcc("Kuzaz", "gvan");
+            Table[8] = new BankAcc("Kuzal", "jvan");
+            Table[9] = new BankAcc("Kuzaj", "avan");
+            account.shinfo(1);
         }
     }
 }
---
+++ 336Labs/didikova/Bank.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.didikova
+{
+    class Account
+    {
+        private static double _rate;
+        private double _paymentac;
+        public string _name;
+        public string _surname;
+        int _old;
+        private int[] _id = { };
+        public void spisok(string sur, string nam)
+        {
+            nam = nam.Trim();
+            var firstLet = nam[0];
+            var lastLet = nam.Remove(0, 1);
+            _name = firstLet.ToString().ToUpper() + lastLet;
+
+            sur = sur.Trim();
+            var firstLet1 = sur[0];
+            var lastLet1 = sur.Remove(0, 1);
+            _surname = firstLet1.ToString().ToUpper() + lastLet1;
+
+
+        }
+        public void shinfo(int j)
+        {
+            BankAcc[] reg = new BankAcc[10];
+            for (int i = 0; i < reg.Length; i++)
+            {
+                if (j == i)
+                {
+                    Console.WriteLine(_name + " " + _surname);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Getid(string us)
+        {
+            Random rnd = new Random();
+            string[] Accid = { };
+
+            for (int j = 0; j < Accid.Length; j++)
+            {
+                if (_surname == us)
+                {
+                    for (int i = 0; i < _id.Length; i++)
+                    {
+                        if (i == j)
+                            Console.WriteLine(_id[i]);
+                        else
+                        {
+                            if (i == _id.Length)
+                            {
+                                _id[i] = rnd.Next(000000, 100000);
+                                Console.WriteLine(_id[i]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void Getold()
+        {
+            //DateTime;
+        }
+    }
+    class BankAcc
+    {
+        public BankAcc(string sur, string nam)
+        {
+            Account account = new Account();
+            account._surname = sur;
+            account._name = nam;
+        }
+
+    }
+}
