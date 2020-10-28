512a9f8e07ea4be49e9355b13ee2c18f4a138872
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-10-28 11:06:30 +0300

    commit fix 28.10.2020

--- 336Labs/Farkhutdinov/BankAccount.cs
+++ 336Labs/Farkhutdinov/BankAccount.cs
@@ -9,9 +9,8 @@ namespace _336Labs.Farkhutdinov
     {
         private string _name;
         private string _surname;
-        private static int _id = Environment.TickCount;
+        private static int _id;
         private static double _rate = 7.7;
-
         public void SetName(string newName)
         {
             newName = newName.Trim();
@@ -19,7 +18,6 @@ namespace _336Labs.Farkhutdinov
             var otherLetters = newName.Remove(0, 1);
             _name = firstLetters.ToString().ToUpper() + otherLetters;
         }
-
         public void SetSurName(string newSurName)
         {
             newSurName = newSurName.Trim();
@@ -27,11 +25,43 @@ namespace _336Labs.Farkhutdinov
             var otherLetters = newSurName.Remove(0, 1);
             _surname = firstLetters.ToString().ToUpper() + otherLetters;
         }
-
-        public int personid
+        public void SetRate(double newRate)
         {
-            get { return Interlocked.Increment(ref _id); }
+            _rate = newRate;
+        }
+        public void SetID()
+        {
+            Random rnd = new Random();
+            int[] _id = { };
+            for (int i = 0; i < 20; i++)
+            {
+                int temp = rnd.Next(1, 20);
+                _id[i] = temp;
+            }
+        }
+        public void GetName()
+        {
+            Console.WriteLine($"{_name}");
+        }
+        public void GetSurName()
+        {
+            Console.WriteLine($"{_surname}");
+        }
+        public void GetID()
+        {
+            Console.WriteLine($"{_id}");
+        }
+        public void GetRate()
+        {
+            Console.WriteLine($"{_rate}");
         }
 
+        public void GetInfo()
+        {
+            Console.WriteLine($" Имя - {_name}");
+            Console.WriteLine($" Фамилия - {_surname}");
+            Console.WriteLine($" Уникальный id - {_id}");
+            Console.WriteLine($" Процентная ставка - {_rate}");
+        }
     }
 }
