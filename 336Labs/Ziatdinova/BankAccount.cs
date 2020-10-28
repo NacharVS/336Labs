1add6f4935a5d6888cc10bf2f29c3686f84f83ce
Kolpakova336 <kke2003@mail.ru>
2020-10-28 11:03:15 +0300

    Bank Account fix

--- 336Labs/Ziatdinova/BankAccount.cs
+++ 336Labs/Ziatdinova/BankAccount.cs
@@ -8,13 +8,25 @@ namespace _336Labs.Ziatdinova
     {
         private string _name;
         private string _SURNAME;
-        private string _id;
+        private int[] _id = { };
         private static double _rate;
         private double _paymentAccount;
+        public void list(string sur, string nam)
+        {
+            nam = nam.Trim();
+            var oneLet = nam[0];
+            var lastLet = nam.Remove(0, 1);
+            _name = oneLet.ToString().ToUpper() + lastLet;
+
+            sur = sur.Trim();
+            var oneLet1 = sur[0];
+            var lastLet1 = sur.Remove(0, 1);
+            _SURNAME = oneLet1.ToString().ToUpper() + lastLet1;
+        }
 
         public void SetName(string newName)
         {
-         ff
+        
         }
     }
 
