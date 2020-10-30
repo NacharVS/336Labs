1e7d08a4f87d00dd1860e3aee2540f762af39210
mkarina900 <karinch900@gmail.com>
2020-10-30 22:53:18 +0300

    StringOperations finish

--- 336Labs/Melenteva/StringOperations.cs
+++ 336Labs/Melenteva/StringOperations.cs
@@ -10,6 +10,25 @@ namespace _336Labs.Melenteva
         {
             Console.WriteLine("Enter a word:");
             string slovo = Console.ReadLine();
+            string oddLetters = "";
+            string evenLetters = "";
+            int parametr = 0;
+            foreach (char item in slovo)
+            {
+                if (parametr % 2 == 0)
+                {
+                    oddLetters += slovo[parametr];
+
+                }
+                else
+                {
+                    evenLetters += slovo[parametr];
+                }
+                parametr++;
+            }
+            Console.WriteLine("oddletters " + oddLetters);
+            Console.WriteLine("evenletters " + evenLetters);
+
         }
     }
 }
