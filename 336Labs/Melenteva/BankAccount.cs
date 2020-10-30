0e660b263179e360409cd84d77f627d4d353ab8a
mkarina900 <karinch900@gmail.com>
2020-10-30 19:21:06 +0300

    BankAccount fix

--- 336Labs/Melenteva/BankAccount.cs
+++ 336Labs/Melenteva/BankAccount.cs
@@ -6,5 +6,17 @@ namespace _336Labs.Melenteva
 {
     class BankAccount
     {
-    }
+        private string _name;
+        private string _surname;
+        private static int _id;
+        public static double _rate;
+        private double _paymentAccount;
+        public void SetName(string nName)
+        {
+            nName = nName.Trim();
+            var firstLetter = nName[0];
+            var oLetters = nName.Remove(0, 1);
+
+
+        }
 }
