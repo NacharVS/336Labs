da019b67a07666b3cd67510f2b0401a954528697
Sogorin <Sogorin_ivan@mail.ru>
2020-10-30 09:35:35 +0300

    v0.00000000001

--- 336Labs/Sogorin/Abctactclass.cs
+++ 336Labs/Sogorin/Abctactclass.cs
@@ -6,5 +6,30 @@ namespace _336Labs.Sogorin
 {
     class Abctactclass
     {
+        abstract class Info
+        {            
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public Info(string surname, string name)
+            {
+                surname = Surname;
+                name = Name;
+            }
+
+            public void shInfo()
+            {
+                Console.WriteLine($"{Surname} {Name}");
+            }
+        }
+        class Acc : Info
+        {
+            public int Old { get; set; }
+
+            public Client(string name, int old)
+                : base(name)
+            {
+                Old = old;
+            }
+        }
     }
 }
