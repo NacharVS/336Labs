6ebc817e30b1b17a096782ee704f95c5b13a9f0e
mkarina900 <karinch900@gmail.com>
2020-10-30 22:31:58 +0300

    BankAccount finish

--- 336Labs/Melenteva/BankAccount.cs
+++ 336Labs/Melenteva/BankAccount.cs
@@ -16,7 +16,77 @@ namespace _336Labs.Melenteva
             nName = nName.Trim();
             var firstLetter = nName[0];
             var oLetters = nName.Remove(0, 1);
-            _surname = firstLetter.ToString().ToUpper() + oLetters;
+            _name = firstLetter.ToString().ToUpper() + oLetters;
 
         }
+        public void SetSurname(string nSurname)
+        {
+            nSurname = nSurname.Trim();
+            var firstletter = nSurname[0];
+            var oLetters = nSurname.Remove(0, 1);
+            _surname = firstletter.ToString().ToUpper() + oLetters;
+
+        }
+        public void SetId()
+        {
+            int a = 0;
+            string ID = "";
+            for (int i = 0; i < 3; i++)
+            {
+                ID = (char)(a % 10 + '0') + ID;
+                a = a - 10;
+            }
+              _id = ID;
+
+        }
+        public double GetRate()
+        {
+            return _rate;
+        }
+        public void SetRate()
+        {
+            _rate = 5.4;
+        }
+        public void SetPaymentAccount(double nPaymentAccount)
+        {
+            _paymentAccount = nPaymentAccount;
+        }
+        public double GetPaymentAccount()
+        {
+            return _paymentAccount;
+        }
+        public void SetSumm(double nPaymentAccount)
+        {
+            _paymentAccount = _paymentAccount + nPaymentAccount;
+            Console.WriteLine(_paymentAccount);
+        }
+        public void SetMinus(double nPaymentAccount)
+        {
+            _paymentAccount = _paymentAccount - nPaymentAccount;
+            Console.WriteLine(_paymentAccount);
+        }
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Enter your name: ");
+            account.SetName(Console.ReadLine());
+            Console.WriteLine("Enter your Surname: ");
+            account.SetSurname(Console.ReadLine());
+            Console.WriteLine("Enter ID: ");
+            account.SetId();
+            Console.WriteLine(account.GetRate());
+            Console.WriteLine("Your account: ");
+            account.SetPaymentAccount(double.Parse(Console.ReadLine()));
+            Console.WriteLine("How much do you put? ");
+            account.SetSumm(double.Parse(Console.ReadLine()));
+            Console.WriteLine(" How much do you want to rent? ");
+            account.SetMinus(double.Parse(Console.ReadLine()));
+
+
+
+        }
+
+    }
+
+
 }
