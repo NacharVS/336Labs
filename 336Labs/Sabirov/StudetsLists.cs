ebc917b381251313c4c51f8131458562784ffa50
SabirovI <insaf.sabirov.02@mail.ru>
2020-11-09 15:00:27 +0300

    start in lesson

--- 336Labs/Sabirov/StudetsLists.cs
+++ 336Labs/Sabirov/StudetsLists.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace _336Labs.Sabirov
-{
-    class StudetsLists
-    {
-        public string _name;
-        public double _MathMark;
-        public double _physicsMark;
-        public double _chemistryMark;
+//namespace _336Labs.Sabirov
+//{
+//    class StudetsLists
+//    {
+//        public string _name;
+//        public double _MathMark;
+//        public double _physicsMark;
+//        public double _chemistryMark;
 
-        public StudetsLists(string name, double math, double physics, double chemitry)
-        {
-            _name = name;
-            _mathMark = math;
-            _physicsMark = physics;
-            _chemistryMark = chemitry;
-        }
+//        public StudetsLists(string name, double math, double physics, double chemitry)
+//        {
+//            _name = name;
+//            _mathMark = math;
+//            _physicsMark = physics;
+//            _chemistryMark = chemitry;
+//        }
         
       
         
-    }
-   class StudentSelection
-    {
-        static void Selection(StudetsLists[] lists , double AvarageMark)
-        {
-            for (int i = 0; i < lists.Length: i++)
-            {
-                if ((lists{i}._mathMark + lists{i}.physicsMark + lists{i}._chemistryMark) / 3 >= AvarageMark)
-                {
+//    }
+//   class StudentSelection
+//    {
+//        static void Selection(StudetsLists[] lists , double AvarageMark)
+//        {
+//            for (int i = 0; i < lists.Length: i++)
+//            {
+//                if ((lists{i}._mathMark + lists{i}.physicsMark + lists{i}._chemistryMark) / 3 >= AvarageMark)
+//                {
 
-                }
-            }
-        }
-    }
+//                }
+//            }
+//        }
+//    }
        
-}
+//}
