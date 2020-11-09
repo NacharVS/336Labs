308a5f7b6e75fab5db73fd8e2b3ad979c58341b2
TanyaBitkova336 <tanyabitkova116@gmail.com>
2020-11-09 22:20:50 +0300

    Delegate fix

--- 336Labs/Bitkova/Deligates/Deligate.cs
+++ 336Labs/Bitkova/Deligates/Deligate.cs
@@ -9,10 +9,10 @@ namespace _336Labs.Bitkova.Deligates
         public static void SortArray()
         {
             Random rnd = new Random();
-            int[] array = new int[7];
+            int[] array = new int[5];
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rnd.Next(0, 50);
+                array[i] = rnd.Next(0, 10);
             }
 
             int temp;
@@ -31,28 +31,97 @@ namespace _336Labs.Bitkova.Deligates
 
             for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(array[i]);
+                Console.WriteLine("Сортировка массива - " + array[i]);
             }
         }
-        public void ElementSum()
+        public static void ElementSum()
         {
+            Random rnd = new Random();
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+            }
 
+            int sum = 0;
+            foreach (int value in array)
+            {
+                sum += value;
+                Console.WriteLine(array);
+            }
+            
+            Console.WriteLine("Сумма элеиентов массива - " + sum);
         }
-        public void MaxArray()
+        public static void MaxArray()
         {
-
+            Random rnd = new Random();
+            int[] array = new int[5];
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+                if (max < array[i]) max = array[i];
+                Console.WriteLine(array);
+            }
+            
+            Console.WriteLine("Максимальный элемент массива - " + max);
         }
-        public void sum()
+        public static void Sum()
         {
-
+            Random rnd = new Random();
+            int[] array = new int[5];
+            int[] array2 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+                Console.WriteLine(array);
+                array2[i] = rnd.Next(0, 10);
+                Console.WriteLine(array2);
+            }
+            int[] array3 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array3[i] = array[i] + array2[i];
+                Console.WriteLine("Сумма двух массивов" + array3[i]);
+            }
         }
-        public void minus()
+        public static void Minus()
         {
-
+            Random rnd = new Random();
+            int[] array = new int[5];
+            int[] array2 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+                Console.WriteLine(array);
+                array2[i] = rnd.Next(0, 10);
+                Console.WriteLine(array2);
+            }
+            int[] array3 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array3[i] = array[i] - array2[i];
+                Console.WriteLine("Разность двух массивов" + array3[i]);
+            }
         }
-        public void Multiplus()
+        public static void Multiplus()
         {
-
+            Random rnd = new Random();
+            int[] array = new int[5];
+            int[] array2 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+                Console.WriteLine(array);
+                array2[i] = rnd.Next(0, 10);
+                Console.WriteLine(array2);
+            }
+            int[] array3 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array3[i] = array[i] * array2[i];
+                Console.WriteLine("Умножение двух массивов" + array3[i]);
+            }
         }
     }
 }
--- 336Labs/Bitkova/Deligates/Fillial.cs
+++ 336Labs/Bitkova/Deligates/Fillial.cs
@@ -10,7 +10,18 @@ namespace _336Labs.Bitkova.Deligates
         static void Main(string[] args)
         {
             Trainee tr1;
-            tr1 = Deligate.
+            tr1 = Deligate.SortArray;
+            tr1();
+            tr1 = Deligate.ElementSum;
+            tr1();
+            tr1 = Deligate.MaxArray;
+            tr1();
+            tr1 = Deligate.Sum;
+            tr1();
+            tr1 = Deligate.Minus;
+            tr1();
+            tr1 = Deligate.Multiplus;
+            tr1();
 
         }
 
