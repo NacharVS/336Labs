72f865f6e10dda5f5e46299d499b07a930f7641b
Podolskiy <daniilpodolskiy@mail.ru>
2020-11-09 15:10:38 +0300

    fix

---
+++ 336Labs/Podolskiy/Deligates/Filial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Podolskiy.Deligates
+{
+    class Filial
+    {
+        private delegate void Calculator();
+      static void   Main(String [] args )
+        {
+            Calculator calc1;
+            Calculator calc2;
+          
+        }
+     
+    }
+}
---
+++ 336Labs/Podolskiy/Deligates/General.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Podolskiy.Deligates
+{
+    class General
+    {
+        
+       public static void Sort(int[] mass)
+        {
+            
+            Random rnd = new Random();
+            int temp;
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                mass[i] = rnd.Next(0,100);
+            
+                 for (int n = i+1; n <mass.Length;n++)
+                {
+                    if (mass[i]> mass[n])
+                    {
+                        temp = mass[i];
+                        mass[i] = mass[n];
+                        mass[n] = temp; 
+                    }
+                }
+            }
+            return; 
+        }
+    }
+}
