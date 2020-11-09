e0a3461ed2160783fc8d3902d6b90d9967208201
VS1V <al11002@yndex.ru>
2020-11-09 15:08:30 +0300

    delegate fix 1

---
+++ 336Labs/Isaev/Base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _336Labs.Loginov.branсh;
+
+namespace _336Labs.Loginov
+{
+    class Base
+    {
+        private delegate void Trainee(int[] mes);
+        public static void B()
+        {
+            int[] array1 = new int[10];
+            Trainee tr1;
+            Branch.GenerationArray(array1, 0, 10);
+            tr1 = Branch.SortArray;
+            tr1 += Branch.SumArray;
+            tr1 += Branch.MaxArray;
+            tr1(array1);
+        }
+    }
+}
---
+++ 336Labs/Isaev/branсh/branch.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Loginov.branсh
+{
+    class Branch
+    {
+        public static void GenerationArray(int[] arr, int MinN, int MaxN)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                    arr[i] = rnd.Next(MinN, MaxN);
+                    Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+        public static void SortArray(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void SumArray(int[] arr)
+        {
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp = temp + arr[i];
+            }
+            Console.WriteLine($"{temp}");
+        }
+
+        public static void MaxArray(int[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                    {
+                    max = max + arr[i];
+                    }
+            }
+            Console.WriteLine($"{max}");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
