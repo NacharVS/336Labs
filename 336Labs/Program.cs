798a329b0c8b943cca61e67ce311ddfe67655f94
Sogorin <Sogorin_ivan@mail.ru>
2020-11-09 14:35:01 +0300

    v0.2

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -4,10 +4,15 @@ namespace _336Labs.Sogorin
 {
     class Program
     {
+        private delegate void Trainee(string mes);
+
         static void Main(string[] args)
         {
             Account account = new Account();
             account.shinfo(1);
+            Trainee tr1;
+            tr1 = Deligate.firstless;
+            tr1("Branch");
         }        
     }   
 }
--- 336Labs/Sogorin/Deligate.cs
+++ 336Labs/Sogorin/Deligate.cs
@@ -6,6 +6,13 @@ namespace _336Labs.Sogorin
 {
     class Deligate
     {
- 
+        public static void firstless(string message)
+        {
+            Console.WriteLine($" {message}");
+        }
+        public static void secless(string message)
+        {
+            Console.WriteLine($" {message}");
+        }
     }
 }
