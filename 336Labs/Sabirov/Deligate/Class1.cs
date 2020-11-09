dfc073446554cc325c9996b2bac73b4ec9c01afd
SabirovI <insaf.sabirov.02@mail.ru>
2020-11-09 15:09:10 +0300

    start in lesson

---
+++ 336Labs/Sabirov/Deligate/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Sabirov.Deligate
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[,] massive = new int[5, 5];
+            Random rnd = new Random();
+
+            for (int g = 0; g < massive.GetLength(0): g++)
+            {
+                for (int i = 0; i < massive.GetLength(0): g++)
+                {
+                    massive[g, i] = rnd.Next(10, 100);
+                    Console.WriteLine($"{massive[g, i]");
+                }
+            }
+        }
+    }
+}
