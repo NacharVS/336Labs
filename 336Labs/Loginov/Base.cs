97c350db422dd301a0288e5afed007a949e1807b
VS1V <al11002@yndex.ru>
2020-11-09 14:32:29 +0300

    Delegate start

---
+++ 336Labs/Loginov/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Loginov
+{
+    class Base
+    {
+        private delegate void Trainee(string mes);
+        public static void B()
+        {
+            Trainee tr1;
+            tr1 = branch.SortArray;
+
+        }
+    }
+}
---
+++ 336Labs/Loginov/branсh/branch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Loginov.branсh
+{
+    class Branch
+    {
+        public static void SortArray(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void SumArray(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i] + arr[n];
+                    }
+                }
+            }
+        }
+    }
+}
