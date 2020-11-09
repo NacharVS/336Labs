e2c229252fb29b48dd6886b2287f27b1d16631ef
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-11-09 15:07:55 +0300

    meet fix

--- 336Labs/Ippolitova/Delegates/GeneralOffice.cs
+++ 336Labs/Ippolitova/Delegates/GeneralOffice.cs
@@ -6,5 +6,26 @@ namespace _336Labs.Ippolitova.Delegates
 {
     class GeneralOffice
     {
-    }
+         public static void Max(string[] args)
+         {
+            int[] mass = new int[5];
+            Random rnd = new Random();
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(0, 100);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                if (mass[i] > mass[i + 1])
+                    Console.WriteLine($" {mass[i]}!");
+            }
+         }
+
+        public static void Sorted(string[] args)
+        {
+
+        }
+    }  
 }
