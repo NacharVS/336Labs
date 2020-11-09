500db9a5f32c847ba0d7ac2f214c0f2af605fec1
Amir1902 <amir_galimzyanov@mail.ru>
2020-11-09 14:52:02 +0300

    fix

---
+++ 336Labs/Deligates/General.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Deligates
+{
+    class General
+    {
+        public void Sort()
+        { 
+        
+        }
+        public void Sum()
+        {
+
+        }
+        public void Max()
+        {
+
+        }
+        public void Sumdva()
+        {
+
+        }
+        public void Razn()
+        {
+
+        }
+        public void Proizv()
+        {
+
+        }
+        
+    }
+}
