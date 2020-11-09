c6e6a22176e3296134029312212201e6ec1965cf
Amir1902 <amir_galimzyanov@mail.ru>
2020-11-09 15:03:15 +0300

    Fix

---
+++ 336Labs/Deligates/General.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Deligates
+{
+    class General
+    {
+        public void sort()
+        {
+
+        }
+        public void sum()
+        {
+
+        }
+        public void max()
+        {
+
+        }
+        public void sum2()
+        {
+
+        }
+        public void raznost()
+        {
+
+        }
+        public void proizvedenie()
+        {
+
+        }
+    }
+}
