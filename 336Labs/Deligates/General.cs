38218b35ce126d5db592bdef98abf64a0cfe95b6
AsadullinAizat336 <ayzat.asadullin2016@yandex.ru>
2020-11-09 23:14:53 +0300

    fix 2

--- 336Labs/Deligates/General.cs
+++ 336Labs/Deligates/General.cs
@@ -6,29 +6,68 @@ namespace _336Labs.Deligates
 {
     class General
     {
-        public void sort()
+        public void sort(int[] array)
         {
-
+            Random rnd = new Random();
+            int tr;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int n = i + 1; n < array.Length; n++)
+                {
+                    if (array[i] > array[n])
+                    {
+                        tr = array[i];
+                        array[i] = array[n];
+                        array[n] = tr;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}");
+            }
+            Console.WriteLine();
         }
-        public void sum()
+        public void sum(int[] array)
         {
-
+            Random rmd = new Random();
+            int tr = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                tr = tr + array[i];
+            }
+            Console.WriteLine($"{tr}");
         }
-        public void max()
+        public void max(int[] array)
         {
-
+            Random rnd = new Random();
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = max + array[i];
+                }
+            }
+            Console.WriteLine($"{max}");
         }
         public void sum2()
         {
-
+            Random rnd = new Random();
         }
         public void raznost()
         {
 
         }
-        public void proizvedenie()
+        public void proizvedenie(int[] a)
         {
-
-        }
+            static double multyF(int n)
+            {
+               
+                double p = 1;
+                for (int k = 0; k < n; k++)
+                    p = p * a[k];
+                return p;
+            }
     }
 }
