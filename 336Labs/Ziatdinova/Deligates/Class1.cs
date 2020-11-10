e75642fc0aca2ee4f21b1c125589ce8636f6c4c8
Kolpakova336 <kke2003@mail.ru>
2020-11-10 11:01:17 +0300

    deligates fix

--- 336Labs/Ziatdinova/Deligates/Class1.cs
+++ 336Labs/Ziatdinova/Deligates/Class1.cs
@@ -6,12 +6,40 @@ namespace _336Labs.Ziatdinova.Deligaties
 {
     class Class1
     {
-        private delegate void Trainee(string mes);
-
-        static void Main(string[] args)
+        public static void GenerationArray(int[] arr, int Min, int Max)
         {
-            dcghbjnvgbb 
-
+            Random rndm = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                arr[i] = rndm.Next(Min, Max);
+                Console.WriteLine($"(arr[i])");
+            }
+            Console.WriteLine();
         }
+        public static void SortArray(int[] arr)
+        {
+            int tem;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        tem = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = tem;
+                    }
+                }
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine();
+
+
+
+            }   
+        }
+
     }
 }
