f398c55c2c25969235a21b145a1093f3653e16bc
VS1V <al11002@yndex.ru>
2020-11-10 10:23:14 +0300

    Delegate finish v2

--- 336Labs/Loginov/branсh/branch.cs
+++ 336Labs/Loginov/branсh/branch.cs
@@ -70,32 +70,35 @@ namespace _336Labs.Loginov.branсh
     {
         public static void SumTwoArray(int[] arr1, int[] arr2)
         {
+            int[] arr3 = new int[10];
             Console.Write("SumTwo:  ");
             for (int i = 0; i < arr1.Length; i++)
             {
-                arr1[i] = arr1[i] + arr2[i];
-                Console.Write($"{arr1[i]} ");
+                arr3[i] = arr1[i] + arr2[i];
+                Console.Write($"{arr3[i]} ");
             }
             Console.WriteLine();
         }
 
         public static void DiffTwoArray(int[] arr1, int[] arr2)
         {
+            int[] arr3 = new int[10];
             Console.Write("DiffTwo: ");
             for (int i = 0; i < arr1.Length; i++)
             {
-                arr1[i] = arr1[i] - arr2[i];
+                arr3[i] = arr1[i] - arr2[i];
                 Console.Write($"{arr1[i]} ");
             }
             Console.WriteLine();
         }
         public static void MultTwoArray(int[] arr1, int[] arr2)
         {
+            int[] arr3 = new int[10];
             Console.Write("MultTwo: ");
             for (int i = 0; i < arr1.Length; i++)
             {
-                arr1[i] = arr1[i] * arr2[i];
-                Console.Write($"{arr1[i]} ");
+                arr3[i] = arr1[i] * arr2[i];
+                Console.Write($"{arr3[i]} ");
             }
             Console.WriteLine();
         }
