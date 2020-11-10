9cedc8eced56347e9492f58dfc828e0517a673c4
VS1V <al11002@yandex.ru>
2020-11-10 11:06:13 +0300

    deligate

--- 336Labs/Vasilev/Delegate/BranchOffice.cs
+++ 336Labs/Vasilev/Delegate/BranchOffice.cs
@@ -6,14 +6,24 @@ namespace _336Labs.Vasilev.Deligate
 {
     class BranchOffice
     {
-       public void sort()
-       {
-
-       }
-       public void elementsum()
-       {
-
-       }
-       public void 
+       public static void sum()
+        {
+            Random rnd = new Random();
+            int[] array = new int[5];
+            int[] array2 = new int[5];
+            for (int i=0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+                Console.WriteLine(array);
+                array2[i] = rnd.Next(0, 10);
+                Console.WriteLine(array2);
+            }
+            int[] array3 = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array3[i] = array[i] + array2[i];
+                Console.WriteLine("Сложение массивов" + array3[i]);
+            }
+        }
     }
 }
