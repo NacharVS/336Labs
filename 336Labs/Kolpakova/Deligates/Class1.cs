dd2818b3e5636c652725b4c20823b930a24ae6f2
Kolpakova336 <kke2003@mail.ru>
2020-11-10 11:02:10 +0300

    deligates fix

--- 336Labs/Kolpakova/Deligates/Class1.cs
+++ 336Labs/Kolpakova/Deligates/Class1.cs
@@ -6,15 +6,48 @@ namespace _336Labs.Kolpakova.Deligates
 {
     class Class1
     {
-        private delegate void Trainee(string mes);
-        {
-            static void Main(string[] args)
+        public static void GenerationArray(int[] arr, int Min, int Max)
+        { 
+            Random rndm = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
+                arr[i] = rndm.Next(Min, Max);
+                Console.WriteLine($"(arr[i])");
+            }
+            Console.WriteLine();
+        }
+        public static void SortArray(int[] arr)
+        {
+            int tem;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+               for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        tem = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = tem;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine();
 
             }
+
+            
+        }
+    } 
+    
+
+
+        
         
 
-        }
+        
 
-    }
+
+    
 }
