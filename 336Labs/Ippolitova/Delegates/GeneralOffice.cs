3325c1befe4f8e46bfb84de83cdcfb15002fc639
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-11-10 11:06:52 +0300

    finish

--- 336Labs/Ippolitova/Delegates/GeneralOffice.cs
+++ 336Labs/Ippolitova/Delegates/GeneralOffice.cs
@@ -6,11 +6,10 @@ namespace _336Labs.Ippolitova.Delegates
 {
     class GeneralOffice
     {
-         public static void Max(string[] args)
+         public static void Max(int[] mass)
          {
-            int[] mass = new int[5];
             Random rnd = new Random();
-
+            int max = 0;
             for (int i = 0; i < mass.Length; i++)
             {
                 mass[i] = rnd.Next(0, 100);
@@ -18,14 +17,107 @@ namespace _336Labs.Ippolitova.Delegates
             Console.WriteLine();
             for (int i = 0; i < mass.Length-1; i++)
             {
-                if (mass[i] > mass[i + 1])
-                    Console.WriteLine($" {mass[i]}!");
+                if (mass[i] > max)
+                    max = mass[i];
             }
+            Console.WriteLine(max);
          }
-
-        public static void Sorted(string[] args)
+        
+        public static void Sorted(int[] mass)
         {
-            if 
+            Random rnd = new Random();
+            int sort = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(0, 100);
+            }
+            Console.WriteLine();
+            for (int j = 0; j < mass.Length; j++)
+            {
+
+
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i] > mass[i + 1])
+                    {
+                        sort = mass[i + 1];
+                        mass[i + 1] = mass[i];
+                        mass[i] = sort;
+                    }
+
+                }
+            }
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+                
+        }
+
+        public static void Summ(int[] mass)
+        {
+            Random rnd = new Random();
+            int summ = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(0, 100);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                if (mass[i] > 0)
+                    summ += mass[i];
+            }
+            Console.WriteLine(summ);
+        }
+
+        public static void SummMass(int[] mass, int[] mass1)
+        {
+            Random rnd = new Random();
+            
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(0, 100);
+                mass1[i] = rnd.Next(0, 100);
+                mass[i] = mass[i] + mass1[i];
+            }
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+        }
+            public static void DiffMass(int[] mass, int[] mass1)
+            {
+                Random rnd = new Random();
+                
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i] = rnd.Next(0, 100);
+                    mass1[i] = rnd.Next(0, 100);
+                    mass[i] = mass[i] - mass1[i];
+                }
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    Console.WriteLine(mass[i]);
+                }
+
+            }
+
+        public static void MultiMass(int[] mass, int[] mass1)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(0, 100);
+                mass1[i] = rnd.Next(0, 100);
+                mass[i] = mass[i] * mass1[i];
+            }
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+
         }
     }  
 }
