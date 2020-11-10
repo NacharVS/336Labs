1ccf25d4f1ba998b6d5f2fb44e9fa2b72835a15b
Liana949 <bekmuratovaliana2002@gmail.com>
2020-11-10 11:04:43 +0300

    Deligate fix

--- 336Labs/Melenteva/Deligate.cs
+++ 336Labs/Melenteva/Deligate.cs
@@ -6,5 +6,36 @@ namespace _336Labs.Melenteva
 {
     class Deligate
     {
+        public static void GenerationArray(int[] arr, int Min, int Max)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(Min, Max);
+                Console.Write($"{arr[i]} ");
+        }
+            Console.WriteLine();
     }
+        public static void SortArray(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+                {
+                    Console.WriteLine();
+                }
+        }
 }
