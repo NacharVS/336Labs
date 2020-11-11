62d95d61dd764585156f6bf269eb0bae537db62e
Podolskiy <daniilpodolskiy@mail.ru>
2020-11-11 11:06:31 +0300

    start

--- 336Labs/Podolskiy/BankAccount.cs
+++ 336Labs/Podolskiy/BankAccount.cs
@@ -1,47 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace _336Labs.Podolskiy
-{
-    class BankAccount
-    {
-        private string _name;
-        private string _surname;
-        private string _id;
-        private static double _rate = 0.067;
-        public static double _paymentAccount;
+//namespace _336Labs.Podolskiy
+//{
+//    class BankAccount
+//    {
+//        private string _name;
+//        private string _surname;
+//        private string _id;
+//        private static double _rate = 0.021;
+//        private double _paymentAccount = 12000;
 
+//        public void SetName(string newName)
+//        {
+//            newName = newName.Trim();
+//            var firstLetter = newName[0];
+//            var otherLetter = newName.Remove(0, 1);
+//            _name = firstLetter.ToString().ToUpper() + otherLetter;
+//            _surname = firstLetter.ToString().ToUpper() + otherLetter;
+//            Console.WriteLine(_name);
 
-        public void setName(string newName)
-        {
-            newName = newName.Trim();
-            var firstLetter = newName[0];
-            var otherLetters = newName.Remove(0, 1);
-            _name = firstLetter.ToString().ToUpper() + otherLetters;
+//        }
+//        public void Deposit()
+//        {
+//            Console.WriteLine("Сколько хотите внести >>> ");
 
-        }
-        public void Deposit(double deposit)
-        {
+//            double depos = Convert.ToDouble(Console.ReadLine());
 
-            _paymentAccount = _paymentAccount + deposit;
-            Console.WriteLine("На вашем счету " + _paymentAccount + " рублей ");
-        }
-        public void Withdraw(double withdraw)
-        {
-            _paymentAccount = _paymentAccount - withdraw;
-            Console.WriteLine("На вашем счету " + _paymentAccount + " рублей ");
+//            _paymentAccount = _paymentAccount + depos;
 
-        }
-        static void Main(string[] args)
-        {
-            
-            
+//            Console.WriteLine("На вашем счету стало " + _paymentAccount);
+//        }
 
-            _paymentAccount = 100000;
-            bank.Deposit(1000);
-            bank.Withdraw(1500); 
-            
-        }
-    } 
-}
+//        public void Withdraw()
+//        {
+//            Console.WriteLine("Сколько хотите снять  >>> ");
+//            double withdraw = Convert.ToDouble(Console.ReadLine());
+//            _paymentAccount = _paymentAccount - withdraw;
+
+//            Console.WriteLine("На вашем счету осталось : " + _paymentAccount);
+//        }
+
+//        public void MethodDates()
+//        {
+//            DateTime dayOfBirthday = new DateTime();
+//            int toYear = DateTime.Now.Year;
+
+//            Console.WriteLine("Введите год:");
+//            int years = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Введите месяц:");
+//            int month = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Введите день:");
+//            int days = int.Parse(Console.ReadLine());
+
+//            dayOfBirthday.AddYears(years);
+//            dayOfBirthday.AddMonths(month);
+//            dayOfBirthday.AddDays(days);
+
+//            Console.WriteLine("Вам сейчас: ");
+//            Console.WriteLine(toYear - years);
+//        }
+//    }
+//}
