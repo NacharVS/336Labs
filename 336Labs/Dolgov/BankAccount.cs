eca0b41008ee5bd7dc08020c9c7c10b4b51d9ebf
Dolgov336 <dolgov20024@mail.ru>
2020-11-13 11:06:10 +0300

    BankAcc fix

--- 336Labs/Dolgov/BankAccount.cs
+++ 336Labs/Dolgov/BankAccount.cs
@@ -87,6 +87,44 @@ namespace _336Labs.Dolgov
                 return true;
             }
         }
+        class AccountEventArgs
+        {
+            public string Message { get; }
+            public int Sum { get; }
+            public AccountEventArgs(string mes, int sum)
+            {
+                Message = mes;
+                Sum = sum;
+            }
+        }
+        class Account
+        {
+            public delegate void AccountHandler(object sender, AccountEventArgs e);
+            public event AccountHandler Notify;
+            public Account(int sum)
+            {
+                Sum = sum;
+            }
+            public int Sum { get; private set; }
+            public void Put(int sum)
+            {
+                Sum += sum;
+                Notify?.Invoke(this, new AccountEventArgs($"На счет поступило {sum}", sum));
+            }
+            public void Take(int sum)
+            {
+                if (Sum >= sum)
+                {
+                    Sum -= sum;
+                    Notify?.Invoke(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
+                }
+                else
+                {
+                    Notify?.Invoke(this, new AccountEventArgs("Недостаточно денег на счете", sum)); ;
+                }
+            }
+        }
+
         public void Dep()
         {
             Console.WriteLine("Какую сумму хотите положить на счет?");
