aa9e87f6e882d6118485a6fbe0fd7c846968603e
Podolskiy <daniilpodolskiy@mail.ru>
2020-11-13 10:55:50 +0300

    fix

--- 336Labs/Podolskiy/BankAccount.cs
+++ 336Labs/Podolskiy/BankAccount.cs
@@ -1,65 +1,82 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace _336Labs.Podolskiy
-//{
-//    class BankAccount
-//    {
-//        private string _name;
-//        private string _surname;
-//        private string _id;
-//        private static double _rate = 0.021;
-//        private double _paymentAccount = 12000;
+namespace _336Labs.Podolskiy
+{
+    class BankAccount
+    {
 
-//        public void SetName(string newName)
-//        {
-//            newName = newName.Trim();
-//            var firstLetter = newName[0];
-//            var otherLetter = newName.Remove(0, 1);
-//            _name = firstLetter.ToString().ToUpper() + otherLetter;
-//            _surname = firstLetter.ToString().ToUpper() + otherLetter;
-//            Console.WriteLine(_name);
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static double _rate = 0.021;
+        private double _paymentAccount = 12000;
 
-//        }
-//        public void Deposit()
-//        {
-//            Console.WriteLine("Сколько хотите внести >>> ");
 
-//            double depos = Convert.ToDouble(Console.ReadLine());
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetter = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetter;
+        }
+        public void SetSurname(string newSurname)
+        {
+            newSurname = newSurname.Trim();
+            var firstLetter = newSurname[0];
+            var otherLetter = newSurname.Remove(0, 1);
+            _surname = firstLetter.ToString().ToUpper() + otherLetter.ToString().ToLower();
+        }
+        public void Deposit()
+        {
+            Console.WriteLine("Сколько хотите внести >>> ");
 
-//            _paymentAccount = _paymentAccount + depos;
+            double depos = Convert.ToDouble(Console.ReadLine());
 
-//            Console.WriteLine("На вашем счету стало " + _paymentAccount);
-//        }
+            _paymentAccount = _paymentAccount + depos;
 
-//        public void Withdraw()
-//        {
-//            Console.WriteLine("Сколько хотите снять  >>> ");
-//            double withdraw = Convert.ToDouble(Console.ReadLine());
-//            _paymentAccount = _paymentAccount - withdraw;
+            Console.WriteLine("На вашем счету стало " + _paymentAccount);
+        }
 
-//            Console.WriteLine("На вашем счету осталось : " + _paymentAccount);
-//        }
+        //public void GenerationID(BankAccount bank )
+        //  {
+        //      Random random = new Random();
+        //      bank._id = random.Next(10, 100);
+        //  }
 
-//        public void MethodDates()
-//        {
-//            DateTime dayOfBirthday = new DateTime();
-//            int toYear = DateTime.Now.Year;
+        public void Withdraw()
+        {
+            Console.WriteLine("Сколько хотите снять  >>> ");
+            double withdraw = Convert.ToDouble(Console.ReadLine());
+            _paymentAccount = _paymentAccount - withdraw;
 
-//            Console.WriteLine("Введите год:");
-//            int years = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Введите месяц:");
-//            int month = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Введите день:");
-//            int days = int.Parse(Console.ReadLine());
+            Console.WriteLine("На вашем счету осталось : " + _paymentAccount);
+        }
+        public void MethodDate()
+        {
+            DateTime dayOfBirthday = new DateTime();
+            int toYear = DateTime.Now.Year;
 
-//            dayOfBirthday.AddYears(years);
-//            dayOfBirthday.AddMonths(month);
-//            dayOfBirthday.AddDays(days);
+            Console.WriteLine("Введите ваш год рождения");
+            int years = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц, когда вы родились");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите день, когда вы родились");
+            int days = int.Parse(Console.ReadLine());
 
-//            Console.WriteLine("Вам сейчас: ");
-//            Console.WriteLine(toYear - years);
-//        }
-//    }
-//}
+            DateTime dayOfBirth = new DateTime(2002, 4, 4);
+
+            dayOfBirth.AddYears(years);
+            dayOfBirth.AddMonths(month);
+            dayOfBirth.AddDays(days);
+
+            Console.WriteLine($"Год: {years} Месяц:{month} День:{days}");
+
+            Console.WriteLine("Вам сейчас: ");
+            Console.WriteLine(toYear - years);
+        }
+
+    }
+
+}
