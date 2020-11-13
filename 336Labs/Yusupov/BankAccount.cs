9007fac2b28797108c5b12f4ca726ebc5de31ed6
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-11-13 11:08:20 +0300

    meet fix

--- 336Labs/Yusupov/BankAccount.cs
+++ 336Labs/Yusupov/BankAccount.cs
@@ -61,5 +61,109 @@ namespace _336Labs.Yusupov
             Console.WriteLine("Вам сейчас: ");
             Console.WriteLine(toYear - years);
         }
+        public delegate void Handler(string phonenumber);
+        private int _sum;
+        private string _phoneNumber;
+        public BankAccount(int sum, string phoneNumber)
+        {
+            _sum = sum;
+            _phoneNumber = phoneNumber;
+        }
+
+        class Account
+        {
+
+            public delegate void AccountHandler(object sender, AccountEventArgs e);
+            public event AccountHandler Notify;
+            public Account(int sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke($"Со счета снято: {sum}");
+            }
+
+            public int Sum { get; private set; }
+            public void Put(int sum)
+            {
+                Notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {Sum}"); ;
+                Sum += sum;
+                Notify?.Invoke(this, new AccountEventArgs($"На счет поступило {sum}", sum));
+            }
+        }
+    }
+
+    public void Take(int sum)
+    {
+        Console.Write("Сколько хотите внести >>> ");
+        if (Sum >= sum)
+        {
+            Sum -= sum;
+            Notify?.Invoke(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
+        }
+        else
+        {
+            Notify?.Invoke(this, new AccountEventArgs("Недостаточно денег на счете", sum)); ;
+        }
+    }
+
+    class BA
+    {
+
+        private static void DisplayMessage(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine($"Сумма транзакции: {e.Sum}");
+            Console.WriteLine(e.Message);
+        }
+        private static void DisplayRedMessage(String message)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+
+        public static void BnkAc()
+
+        {
+            BankAccount bank = new BankAccount();
+            BankAccount.SetNameSurnameIdAge(bank);
+            BankAccount.SetId(bank);
+            BankAccount.SetAge(bank);
+            if (BankAccount.GetNameSurnameIdAgeRate(bank) == true)
+            {
+
+                BankAccount bank = new BankAccount();
+                BankAccount.SetNameSurnameIdAge(bank);
+                BankAccount.SetId(bank);
+                BankAccount.SetAge(bank);
+                if (BankAccount.GetNameSurnameIdAgeRate(bank) == true)
+                {
+                    /*  int n = 0;
+                      while (n == 0)
+                      {
+                          Console.WriteLine();
+                          bank.Deposit();
+                          Console.WriteLine();
+                          bank.Withdraw();
+                      }*/
+
+                    Account acc = new Account(100);
+                    acc.Put(20);    // добавляем на счет 20
+                    Console.WriteLine($"Сумма на счете: {acc.Sum}");
+                    acc.Take(70);   // пытаемся снять со счета 70
+                    Console.WriteLine($"Сумма на счете: {acc.Sum}");
+                    acc.Take(180);  // пытаемся снять со счета 180
+                    Console.WriteLine($"Сумма на счете: {acc.Sum}");
+                    Console.Read();
+                }
+                Account acc = new Account(100);
+                acc.Notify += DisplayMessage;
+                acc.Put(20);
+                acc.Take(70);
+                acc.Take(150);
+                Console.Read();
+            }
+
+        }
     }
 }
