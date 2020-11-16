305d2232fb6aa994d4642f25033568c38218fe73
Kolpakova336 <kke2003@mail.ru>
2020-11-16 20:36:37 +0300

    BankAccount finish

--- 336Labs/Kolpakova/BankAccount.cs
+++ 336Labs/Kolpakova/BankAccount.cs
@@ -8,9 +8,12 @@ namespace _336Labs.Kolpakova
     {
         private string _name;
         private string _SURNAME;
-        private string _id;
+        private int []_id = { };
         private static double _rate;
         private double _paymentAccount;
+        private string _surname;
+
+        public static int Lenght { get; private set; }
 
         public void list(string sur, string nam)
         {
@@ -28,11 +31,24 @@ namespace _336Labs.Kolpakova
 
         public void shinfo(int j)
         {
+            BankAccount acc1 = new BankAccount("Kuzak", "pvan");
+            BankAccount acc2 = new BankAccount("Kuzap", "uvan");
+            BankAccount acc3 = new BankAccount("Kuzam", "tvan");
+            BankAccount acc4 = new BankAccount("Kuzan", "yvan");
+            BankAccount acc5 = new BankAccount("Kuzab", "evan");
 
+            for (int i = 0; i < BankAccount.Lenght; i++)
+            {
+                if (j == 1)
+                {
+                    Console.WriteLine(_name + " " + _surname);
+                    Console.WriteLine();
+                }
+            }
         }
         public void Getid(string us)
         {
-            Random rndm = new Random();
+            Random rnd = new Random();
             string[] Accid = { };
             for (int j = 0; j < Accid.Length; j++)
             {
@@ -46,8 +62,8 @@ namespace _336Labs.Kolpakova
                         {
                             if (i == _id.Length)
                             {
-                                _id[i] = rndm.Next(000000, 100000);
-                                Console.WriteLine;
+                                _id[i] = rnd.Next(000000, 100000);
+                                Console.WriteLine(_id[i]);
                             }
                         }
                     }
@@ -55,5 +71,101 @@ namespace _336Labs.Kolpakova
                
             }
         }    
+    }   
+}   
+class BankAcc
+{
+    public BankAcc(int sum, string phnum)
+    {
+        int _sum = sum;
+        string _phnum = phnum;
     }
+    private int _sum;
+    private string _phnum;
+    public string Notify = "Massage";
+    public int Sum
+    {
+        get
+        {
+            return _sum;
+        }
+        set
+        {
+            _sum = value;
+            //Notify?.Invoke(Phonnum);
+        }
+
+    }
+    public string Phonnum
+    {
+        get
+        {
+            return _phnum;
+        }
+        set
+        {
+            _phnum = value;
+
+        }
+    }
+}
+class BankAcco
+{
+    private string _name;
+    private string _surname;
+    private int[] _id = { };
+    private static double _rate;
+    private double _paymentac;
+    private bool mass;
+
+    public void Setname (string newName)
+    {
+        newName = newName.Trim();
+        var firstLet = newName[0];
+        var lastLet = newName.Remove(0, 1);
+        _name = firstLet.ToString().ToUpper() + lastLet;
+    }
+        public void Setsurname(string newsurName)
+    {
+        newsurName = newsurName.Trim();
+        var firstLet = newsurName[0];
+        var lastLet = newsurName.Remove(0, 1);
+        _surname = firstLet.ToString().ToUpper() + lastLet;
+    }
+      public void Getid(string us)
+    {
+        Random rnd = new Random();
+        string[] Accid = { };
+        for (int j = 0; j < Accid.Length; j++)
+        {
+            if (_surname == us)
+            {
+                for (int i = 0; i < _id.Length; i++)
+                {
+                    if (i == j)
+                        Console.WriteLine(_id[1]);
+                    else
+                    {
+                        if (i == _id.Length)
+                        {
+                            _id[i] = rnd.Next(000000, 100000);
+                            Console.WriteLine(_id[i]);
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+    public void info()
+    {
+        Console.WriteLine(mass);
+    }
+    public void showinfo(int num)
+    {
+        Console.WriteLine(_id[num] + _name[num] + _surname[num]);
+    }
+
+
 }
