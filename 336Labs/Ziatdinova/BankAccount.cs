616c70b2c6c813fdb87c11f7f5d3275fba44f3f7
Kolpakova336 <kke2003@mail.ru>
2020-11-17 10:54:46 +0300

    BankAccount fix

--- 336Labs/Ziatdinova/BankAccount.cs
+++ 336Labs/Ziatdinova/BankAccount.cs
@@ -11,6 +11,18 @@ namespace _336Labs.Ziatdinova
         private int[] _id = { };
         private static double _rate;
         private double _paymentAccount;
+        private string _surname;
+        private string v1;
+        private string v2;
+
+        public BankAccount(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public static int Lenght { get; private set; }
+
         public void list(string sur, string nam)
         {
             nam = nam.Trim();
@@ -26,7 +38,20 @@ namespace _336Labs.Ziatdinova
 
         public void shinfo(int j)
         {
+            BankAccount acc1 = new BankAccount("Kuzak", "pvan");
+            BankAccount acc2 = new BankAccount("Kuzap", "uvan");
+            BankAccount acc3 = new BankAccount("Kuzam", "tvan");
+            BankAccount acc4 = new BankAccount("Kuzan", "yvan");
+            BankAccount acc5 = new BankAccount("Kuzab", "evan");
 
+            for (int i = 0; i < BankAccount.Lenght; i++)
+            {
+                if (j == 1)
+                {
+                    Console.WriteLine(_name + " " + _surname);
+                    Console.WriteLine();
+                }
+            }
         }
 
         public void Getid(string us)
@@ -46,7 +71,7 @@ namespace _336Labs.Ziatdinova
                             if (i == _id.Length)
                             {
                                 _id[i] = rndm.Next(000000, 100000);
-                                Console.WriteLine();
+                                Console.WriteLine(_id[i]);
                             }
                         }
                     }
@@ -60,3 +85,39 @@ namespace _336Labs.Ziatdinova
     
 
 }
+class BankAcc
+{
+    public BankAcc(int sum, string phnum)
+    {
+        int _sum = sum;
+        string _phnum = phnum;
+    }
+    private int _sum;
+    private _phnum;
+    public string Notify = "Message";
+    
+    public int Sum
+    {
+        get
+        {
+            return _sum;
+        }
+        set
+        {
+            _sum = value;
+        }
+    }
+    public string Phonnumm
+    {
+        get
+        {
+            return _phnum;
+        }
+        set
+        {
+            _phnum = value;
+        }
+    }
+}
+
+    
