a6469b483a4a01069ff5095a68aad66f9ad60681
Dolgov336 <dolgov20024@mail.ru>
2020-11-18 10:54:13 +0300

    Br fix

--- 336Labs/Dolgov/Delegates/Br.cs
+++ 336Labs/Dolgov/Delegates/Br.cs
@@ -59,4 +59,42 @@ namespace _336Labs.Dolgov.Branch
             Console.WriteLine($"{max}");
         }
     }
+    class Branch2
+    {
+        public static void SumTwoArray(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[10];
+            Console.Write("SumTwo:  ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr3[i] = arr1[i] + arr2[i];
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void DiffTwoArray(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[10];
+            Console.Write("DiffTwo: ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr3[i] = arr1[i] - arr2[i];
+                Console.Write($"{arr1[i]} ");
+            }
+            Console.WriteLine();
+        }
+        public static void MultTwoArray(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[10];
+            Console.Write("MultTwo: ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr3[i] = arr1[i] * arr2[i];
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
 }
+
