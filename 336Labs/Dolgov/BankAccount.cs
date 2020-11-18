e8b83214f33de98923d59d97ac0abea135a94723
Dolgov336 <dolgov20024@mail.ru>
2020-11-18 10:50:08 +0300

    BankAccount Fix

--- 336Labs/Dolgov/BankAccount.cs
+++ 336Labs/Dolgov/BankAccount.cs
@@ -13,6 +13,7 @@ namespace _336Labs.Dolgov
         public static double _rate = 0.67;
         private double _paymentAccount;
         private DateTime B = new DateTime();
+        public DateTime AgeAc = DateTime.Now;
 
         public void SetName(string newName)
         {
@@ -87,49 +88,56 @@ namespace _336Labs.Dolgov
                 return true;
             }
         }
-        class AccountEventArgs
-        {
-            public string Message { get; }
-            public int Sum { get; }
-            public AccountEventArgs(string mes, int sum)
-            {
-                Message = mes;
-                Sum = sum;
-            }
-        }
-        class Account
-        {
-            public delegate void AccountHandler(object sender, AccountEventArgs e);
-            public event AccountHandler Notify;
-            public Account(int sum)
-            {
-                Sum = sum;
-            }
-            public int Sum { get; private set; }
-            public void Put(int sum)
-            {
-                Sum += sum;
-                Notify?.Invoke(this, new AccountEventArgs($"На счет поступило {sum}", sum));
-            }
-            public void Take(int sum)
-            {
-                if (Sum >= sum)
-                {
-                    Sum -= sum;
-                    Notify?.Invoke(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
-                }
-                else
-                {
-                    Notify?.Invoke(this, new AccountEventArgs("Недостаточно денег на счете", sum)); ;
-                }
-            }
-        }
-       
     }
+    class AEA
+    {
+        public string Message { get; }
+        public int Sum { get; }
+        public AEA(string mes, int sum)
+        {
+            Message = mes;
+            Sum = sum;
+        }
+    }
+    class Account
+    {
+        public delegate void AH(object sender, AEA e);
+        public event AH Notify;
+        public int Sum { get; private set; }
+        public Account(int sum)
+        {
+            Sum = sum;
+        }
+        public void Put(int sum)
+        {
+            Sum += sum;
+            Notify?.Invoke(this, new AEA($"На счет поступило {sum}.", sum));
+        }
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke(this, new AEA($"Сумма {sum} снята со счета.", sum));
+            }
+            else
+            {
+                Notify?.Invoke(this, new AEA("Нехватка средств на счете.", sum)); ;
+            }
+        }
+
+        public void Rate(BankAccount bank)
+        {
+            DateTime Check = DateTime.Now;
+            Check = bank.AgeAc;
+        }
+
+    }
+
     class BankAcco
     {
 
-        private static void DisplayMessage(object sender, AccountEventArgs e)
+        private static void DisplayMessage(object sender, AEA e)
         {
             Console.WriteLine($"Сумма транзакции: {e.Sum}");
             Console.WriteLine(e.Message);
