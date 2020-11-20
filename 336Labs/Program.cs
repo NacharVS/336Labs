5a0d5e322563b00d2fdaf891260b13d02bc64d08
NailYS <nail.yusupov.08@bk.ru>
2020-11-21 00:59:00 +0400

    START INETRFACE

---
+++ 336Labs/Program.cs
@@ -0,0 +1,18 @@
+﻿using _336Labs.Yusupov;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace _336Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWork work1;
+            work1 = new Unit1();
+            work1.CraftEarth(); 
+
+        }
+    }
+}
---
+++ 336Labs/Yusupov/Interfaces/Interfaces.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface IWork 
+    {
+        public double Work  { get; set;  }
+        void CraftEarth(); 
+    }
+    interface IAttack
+    {
+        public double Uron { get; set; }
+        void Attack(); 
+    }
+
+    interface IState
+    {
+        public double High { get; set; }
+        void Bulid(); 
+    }
+    class Unit1 : IWork
+    {
+        double IWork.Work { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        void IWork.CraftEarth()
+        {
+      
+            Console.WriteLine("im unit1 i build this states ");
+        }
+        class Unit2 : IAttack
+        {
+            double IAttack.Uron { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            void IAttack.Attack()
+            {
+                Console.WriteLine("Attack!!! ");
+            }
+        }
+    }
+    }
+
