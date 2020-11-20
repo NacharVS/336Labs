36baf16321052a8cd154fc2c0a995dc049d47d40
Kolpakova336 <kke2003@mail.ru>
2020-11-20 22:54:39 +0300

    Class finish

---
+++ 336Labs/Ziatdinova/Deligates/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Ziatdinova.Deligates
+{
+    class Class2
+    {
+        public static void SumTwoArray(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[10];
+            Console.Write("SumTwo: ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr3[i] = arr1[i] + arr2[i];
+                Console.WriteLine($"{arr3[i]} ");
+            }
+            Console.WriteLine();
+
+        }
+        public static void DiffTwoArray(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[10];
+            Console.Write("DiffTwo: ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr3[i] = arr1[i] + arr2[i];
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
