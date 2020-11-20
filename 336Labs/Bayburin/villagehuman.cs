4dc5eb586c8fe946cab365344cce28f3dbce5a17
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-11-20 11:06:38 +0300

    20.11.2020 на паре

---
+++ 336Labs/Bayburin/villagehuman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Bayburin
+{
+    sealed class villagehuman : Interface1
+    {
+        private double _health;
+        private double _energy;
+
+        public double Health
+        {   get => _health;
+            set => _health = value;
+        }
+
+        public double Energy
+        {
+            get => _energy;
+            set => _energy = value;
+        }
+         
+        public void hel()
+        {
+            Console.WriteLine($"health = {_health} hp");
+        }
+        
+        public void en()
+        {
+            Console.WriteLine($"energy = {_energy}");
+        }
+        
+
+    }
+}
