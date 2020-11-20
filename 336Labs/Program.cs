9bb1e589487326f6fe09a1a6d80a145e641cd9c9
Kolpakova336 <kke2003@mail.ru>
2020-11-20 20:52:03 +0300

    Program finish

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -7,20 +7,26 @@ namespace _336Labs
     {
         static void Main(string[] args)
         {
-            StudentsList Kolpakova = new StudentsList("Kolpakova", 5, 4, 5);
-            StudentsList Ziatdinova = new StudentsList("Ziatdinova", 4, 5, 5);
-            StudentsList Zalyalov = new StudentsList("Zalyalov", 4, 3, 4);
-            StudentsList Galimzyanov = new StudentsList("Galimzyanov", 5, 3, 3);
-            StudentsList Daminov = new StudentsList("Zalyalov", 4, 5, 4);
-            StudentsList Dolgov = new StudentsList("Zalyalov", 3, 2, 5);
-            StudentsList[] studentsLists = new StudentsList[6];
-            studentsLists[0] = Kolpakova;
-            studentsLists[1] = Ziatdinova;
-            studentsLists[2] = Zalyalov;
-            studentsLists[3] = Galimzyanov;
-            studentsLists[4] = Daminov;
-            studentsLists[5] = Dolgov;
-            StudentSelection.Selection(studentsLists, 4);
+            //StudentsList Kolpakova = new StudentsList("Kolpakova", 5, 4, 5);
+            //StudentsList Ziatdinova = new StudentsList("Ziatdinova", 4, 5, 5);
+            //StudentsList Zalyalov = new StudentsList("Zalyalov", 4, 3, 4);
+            //StudentsList Galimzyanov = new StudentsList("Galimzyanov", 5, 3, 3);
+            //StudentsList Daminov = new StudentsList("Zalyalov", 4, 5, 4);
+            //StudentsList Dolgov = new StudentsList("Zalyalov", 3, 2, 5);
+            //StudentsList[] studentsLists = new StudentsList[6];
+            //studentsLists[0] = Kolpakova;
+            //studentsLists[1] = Ziatdinova;
+            //studentsLists[2] = Zalyalov;
+            //studentsLists[3] = Galimzyanov;
+            //studentsLists[4] = Daminov;
+            //studentsLists[5] = Dolgov;
+            //StudentSelection.Selection(studentsLists, 4);
+            //Calculator.Calaculator();
+            //ClassesAndObjects.ClassesAndObjects();
+            //StringOperations.StringOperations();
+            //Class1.Class1();
+            //Class2.Class2();
+            //Bank.Bank();
         }
     }
 }
