e64c8a44dddefd9de8e0f86433478b917ff47c6e
VitaliiLoginov <oblagor@gmail.com>
2020-11-20 10:48:57 +0300

    Interface start

---
+++ 336Labs/Loginov/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Loginov
+{
+    interface IUnits
+    {
+        void Damage();
+    }
+
+    abstract class UnitStats
+    {
+        int health;
+        int defense;
+        int 
+    }
+}
