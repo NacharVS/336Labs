1e0a7270144cc82757018a778a1b689f1edebedc
Amir1902 <amir_galimzyanov@mail.ru>
2020-11-20 11:10:31 +0300

    start interface

---
+++ 336Labs/Acadullin/Peasant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Acadullin
+{
+    interface IPeasant
+    {
+        class peasant : IPeasant
+        {
+            private double _says;
+            public double Says
+            {
+                get => _says;
+                set => _says = value;
+            }
+
+            public void Move()
+            {
+                Console.WriteLine("");
+            }
+
+        }
+
+    }
+}
---
+++ 336Labs/Acadullin/Soldier.cs
@@ -0,0 +1,12 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Acadullin
+{
+    interface ISoldier
+    {
+    }
+}
