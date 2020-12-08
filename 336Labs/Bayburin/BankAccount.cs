fbb70fcff8d5d71514c8f46d1c5e77eafbcd0108
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-12-08 11:04:26 +0300

    1

--- 336Labs/Bayburin/BankAccount.cs
+++ 336Labs/Bayburin/BankAccount.cs
@@ -9,15 +9,39 @@ namespace _336Labs.Bayburin
     {
         private string _name;
         private string _surname;
+        private static string _datebirth;
+        private static int _age;
         private static int _id;
-        private static double _rate = 7.7;
-        private string _phoneNumber;
+        private static double _rate = 0.55;
+        private static double _balance;
+        private static string _phoneNumber;
+        public delegate void Handler(string phoneNumber);
+        public event Handler Notify;
+        public delegate void RateChanged(double oldRate, double newRate);
+        public event RateChanged RateChangedEvent;
+        private DateTime _accountOpenDate;
+        ArrayList ClientList = new ArrayList();
+        public BankAccount(double balance, string phoneNumber)
+        {
+            _balance = balance;
+            _phoneNumber = phoneNumber;
+        }
+        public void SetPhoneNumber(string phoneNumber)
+        {
+            _phoneNumber = phoneNumber;
+            ClientList.Add(_phoneNumber);
+        }
+        public void SetBalance(double balance)
+        {
+            _balance = balance;
+        }
         public void SetName(string newName)
         {
             newName = newName.Trim();
             var firstLetters = newName[0];
             var otherLetters = newName.Remove(0, 1);
             _name = firstLetters.ToString().ToUpper() + otherLetters;
+            ClientList.Add(_name);
         }
         public void SetSurName(string newSurName)
         {
@@ -25,50 +49,75 @@ namespace _336Labs.Bayburin
             var firstLetters = newSurName[0];
             var otherLetters = newSurName.Remove(0, 1);
             _surname = firstLetters.ToString().ToUpper() + otherLetters;
+            ClientList.Add(_surname);
         }
-        public void SetRate(double newRate)
+        public void SetDateBirth(int day, int month, int year)
         {
-            _rate = newRate;
+            _datebirth = ($"{day}.{month}.{year}");
+            if (DateTime.Now.Month >= month && DateTime.Now.Day >= day)
+            { _age = DateTime.Now.Year - year; }
+            else _age = DateTime.Now.Year - year - 1;
+            ClientList.Add(_datebirth);
         }
         public void SetID()
         {
             Random rnd = new Random();
-            int[] _id = { };
-            for (int i = 0; i < 20; i++)
-            {
-                int temp = rnd.Next(1, 20);
-                _id[i] = temp;
-            }
+            _id = rnd.Next(1, 1000000);
+            ClientList.Add(_balance);
         }
-        public void GetName()
-        {
-            Console.WriteLine($"{_name}");
-        }
-        public void GetSurName()
-        {
-            Console.WriteLine($"{_surname}");
-        }
-        public void GetID()
-        {
-            Console.WriteLine($"{_id}");
-        }
-        public void GetRate()
-        {
-            Console.WriteLine($"{_rate}");
-        }
-
         public void GetInfo()
         {
             Console.WriteLine($" Имя - {_name}");
             Console.WriteLine($" Фамилия - {_surname}");
+            Console.WriteLine($" Дата рождения - { _datebirth}");
+            Console.WriteLine($" Ваш возраст - { _age}");
             Console.WriteLine($" Уникальный id - {_id}");
             Console.WriteLine($" Процентная ставка - {_rate}");
+            foreach (var item in ClientList)
+            {
+                Console.WriteLine(item);
+            }
         }
-        public void Events()
+        public void SetBalanceRep(double balance)
         {
+            Balance += balance;
+            ClientList.Add(Balance);
+        }
+        public void SetBalanceRem(double balance)
+        {
+            Balance -= balance;
+            ClientList.Add(Balance);
+        }
+        public void SetRate(double newRate)
+        {
+            Rate = newRate;
+            ClientList.Add(Rate);
         }
         public string PhoneNumber { get; private set; }
-
-        // 18.11.2020
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            private set
+            {
+                _balance = value;
+                Notify?.Invoke(_phoneNumber);
+            }
+        }
+        public double Rate
+        {
+            get
+            {
+                return _rate;
+            }
+            private set
+            {
+                var oldRate = _rate;
+                _rate = value;
+                RateChangedEvent?.Invoke(oldRate, _rate);
+            }
+        }
     }
 }
