5c1af34a474fccdcfa1d48a8bd706a286443cefa
Kolpakova336 <kke2003@mail.ru>
2020-12-09 11:10:12 +0300

    Collection fix

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -5,7 +5,14 @@ namespace _336Labs
 {
     internal class Person
     {
+        private string v1;
+        private string v2;
 
+        public Person(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
     }
     class Program
     {
