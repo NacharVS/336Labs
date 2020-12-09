67ff01c25ce27a224e8d3f8d4096f6e7cebeb933
Podolskiy <daniilpodolskiy@mail.ru>
2020-12-09 11:02:17 +0300

    fix

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿//using System;
 
-namespace _336Labs
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string a = Console.ReadLine();
-            Console.WriteLine("Hi, "+a);
-        }
-    }
-}
+//namespace _336Labs
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string a = Console.ReadLine();
+//            Console.WriteLine("Hi, " + a);
+//        }
+//    }
+//}
