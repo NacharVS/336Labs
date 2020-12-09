5db48d0a597e22ede9792e57a04b3d7ef1d87d1c
Yulia336 <yulya.ippolitova.02@mail.ru>
2020-12-09 10:26:00 +0300

    meet

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -5,19 +5,6 @@ namespace _336Labs
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            ArrayList TestList1 = new ArrayList();
-            ArrayList TestList2 = new ArrayList();
-            {
-                TestList1.Add(Misha);
-                TestList1.Add(Danya);
-                TestList1.Add();
-                TestList1.Add(Misha);
-                TestList1.Add(Misha);
-                TestList1.Add(Misha);
-
-            }
-        }
+       
     }
 }
