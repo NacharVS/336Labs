1fa8724e8da24ca7c2a0af26444b560d1f72bea1
EmilSabirov336 <emil20020102@gmail.com>
2020-12-15 10:59:53 +0300

    lesson start

--- 336Labs/Sabirov/Program.cs
+++ 336Labs/Sabirov/Program.cs
@@ -12,10 +12,34 @@ namespace _336Labs.Sabirov
             lists[0] = new StudetsLists("Masha", 2, 3, 4);
             lists[0] = new StudetsLists("Bulat", 3, 2, 5);
             lists[0] = new StudetsLists("Danil", 4, 3, 4);
+
+
+
+
+
+
+
+
         }
     }
     static double Average(double math, double physics, double chemistry)
     {
-        
+
     }
+    static void WORLD(String[] args)
+    {
+        Dictionary<string, string> countries = new Dictionary<string, string>
+        {
+           {"Франция", "Париж"},
+           {"Германия", "Берлин"},
+           {"Великобритания", "Лондон"}
+        
+        };
+
+        foreach (var pair in countries)
+            Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+    }
+  
+
+
 }
