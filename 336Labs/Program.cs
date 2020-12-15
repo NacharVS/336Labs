6d0175e884f86ba81bbf349ac2d6bfe3b70bfaaf
Sogorin <Sogorin_ivan@mail.ru>
2020-12-15 11:10:22 +0300

    15.12

--- 336Labs/Program.cs
+++ 336Labs/Program.cs
@@ -10,6 +10,7 @@ namespace _336Labs.Sogorin
        
         static void Main(string[] args)
         {
+            /*
             List<String> IdLis = new List<String>();
             List<Account> AccLis = new List<Account>();
             Account acc = new Account();
@@ -18,7 +19,11 @@ namespace _336Labs.Sogorin
             AccLis.Add(new Account("Kuzam", "tvan", 89845535697, "1987, 06, 24", 78782));
             AccLis.Add(new Account("Kuzan", "yvan", 89845256776, "1992, 09, 19", 2982));
             AccLis.Add(new Account("Kuzab", "evan", 89845526875, "1988, 01, 30", 78354));
-            acc.Bank(AccLis, IdLis, acc);            
+            acc.Bank(AccLis, IdLis, acc);   
+            */
+            Slova sl = new Slova();            
+            //List.Add(Dictionary<string, Slova>());
+            sl.Dic(sl);
         }        
     }   
 }
---
+++ 336Labs/Sogorin/Slova.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Sogorin
+{    
+    class Slova
+    {
+        private string _name;
+        private long i_id;
+        private string s_id;
+        public Slova()
+        {
+
+        }
+        public Slova(string nam)
+        {
+            nam = nam.Trim();
+            var firstLet = nam[0];
+            var lastLet = nam.Remove(0, 1);
+            _name = firstLet.ToString().ToUpper() + lastLet;      
+        }
+
+        public void Dic(Slova sl)
+        {
+            Dictionary<string, Slova> List = new Dictionary<string, Slova>();
+            Random rnd = new Random();
+            foreach (var item in List)
+            {
+                i_id = rnd.Next(0, 999999);
+                s_id = i_id.ToString("D6");
+                List.Add(s_id, sl._name);
+            }
+        }
+        
+
+    }
+}
