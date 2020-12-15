b96fb039a64c3497035bc68de0dac76f96762040
AzatBayburin336 <azatbayburin3019@gmail.com>
2020-12-15 11:01:25 +0300

    gg

---
+++ 336Labs/Bayburin/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs.Bayburin
+{
+    class Class1
+    {
+        Dictionary<int, string> student = new Dictionary<int, string>();
+        student.Add();
+    }
+    class GenID
+    {
+        public void GenerId(int id)
+        {
+            string name;
+            int ID;
+
+            Console.ReadLine($"{Dictionary<int, string> student }")
+            Random rnd = new Random();
+
+            int value = rnd.Next();  
+            int value1 = rnd.Next(); 
+            int value2 = rnd.Next(); 
+            int value3 = rnd.Next(); 
+
+            Console.WriteLine(value);  
+            Console.WriteLine(value1); 
+            Console.WriteLine(value2); 
+            Console.WriteLine(value3);
+
+        }
+    }
+}
