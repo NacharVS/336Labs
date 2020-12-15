c6345449f4d8f8a91c7c94348448a0d65b226d26
Liana949 <bekmuratovaliana2002@gmail.com>
2020-12-15 11:05:19 +0300

    dictionary fix

---
+++ 336Labs/dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _336Labs
+{
+    class dictionary
+    {
+        public static void Main(string[] args)
+        {
+            int id = 0;
+            Dictionary<int, string> list = new Dictionary<int, string>();
+            Console.WriteLine("Write you name");
+            while (id < 6)
+            {
+                id++;
+                list.Add(id, Console.ReadLine());
+            }
+
+            foreach (var  item in list)
+            {
+                Console.WriteLine($"id = {item.Key}, Name = {item.Value}");
+            }
+            while (id <= 10)
+            {
+                Console.WriteLine("нвпишите id имени , которое надо заменить");
+                id = Convert.ToInt32(Console.ReadLine());
+                list[id] = Console.ReadLine();
+                Console.WriteLine("список имен");
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"id = {item.Key}, name = {item.Value} ");
+                }
+            }
+        }
+    }
+}
